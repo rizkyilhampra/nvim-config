89e5de1ceaccd44aa12ae91d3fe1df7f1784e778
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-10-16 11:11:29 +0800

    add plugin gitsign and setup

---
+++ lua/aquila/plugins/gitsigns.lua
@@ -0,0 +1,47 @@
+return {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+        require('gitsigns').setup({
+            on_attach                    = function(bufnr)
+                local gs = package.loaded.gitsigns
+
+                local function map(mode, l, r, opts)
+                    opts = opts or {}
+                    opts.buffer = bufnr
+                    vim.keymap.set(mode, l, r, opts)
+                end
+
+                -- Navigation
+                map('n', ']c', function()
+                    if vim.wo.diff then return ']c' end
+                    vim.schedule(function() gs.next_hunk() end)
+                    return '<Ignore>'
+                end, { expr = true })
+
+                map('n', '[c', function()
+                    if vim.wo.diff then return '[c' end
+                    vim.schedule(function() gs.prev_hunk() end)
+                    return '<Ignore>'
+                end, { expr = true })
+
+                -- Actions
+                map('n', '<leader>ghs', gs.stage_hunk)
+                map('n', '<leader>ghr', gs.reset_hunk)
+                map('v', '<leader>ghs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+                map('v', '<leader>ghr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
+                map('n', '<leader>ghS', gs.stage_buffer)
+                map('n', '<leader>ghu', gs.undo_stage_hunk)
+                map('n', '<leader>ghR', gs.reset_buffer)
+                map('n', '<leader>ghp', gs.preview_hunk)
+                map('n', '<leader>ghb', function() gs.blame_line { full = true } end)
+                map('n', '<leader>gtb', gs.toggle_current_line_blame)
+                map('n', '<leader>ghd', gs.diffthis)
+                map('n', '<leader>ghD', function() gs.diffthis('~') end)
+                map('n', '<leader>gtd', gs.toggle_deleted)
+
+                -- Text object
+                map({ 'o', 'x' }, 'gih', ':<C-U>Gitsigns select_hunk<CR>')
+            end
+        })
+    end
+}
