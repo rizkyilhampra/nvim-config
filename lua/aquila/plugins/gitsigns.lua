476e37f1a597f1d2ef9fd053a4cf38be12157fca
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-10-16 11:26:27 +0800

    add desc on every keymap at gitsign plugin

--- lua/aquila/plugins/gitsigns.lua
+++ lua/aquila/plugins/gitsigns.lua
@@ -2,7 +2,7 @@ return {
     'lewis6991/gitsigns.nvim',
     config = function()
         require('gitsigns').setup({
-            on_attach                    = function(bufnr)
+            on_attach = function(bufnr)
                 local gs = package.loaded.gitsigns
 
                 local function map(mode, l, r, opts)
@@ -16,31 +16,33 @@ return {
                     if vim.wo.diff then return ']c' end
                     vim.schedule(function() gs.next_hunk() end)
                     return '<Ignore>'
-                end, { expr = true })
+                end, { expr = true, desc = 'Next hunk'})
 
                 map('n', '[c', function()
                     if vim.wo.diff then return '[c' end
                     vim.schedule(function() gs.prev_hunk() end)
                     return '<Ignore>'
-                end, { expr = true })
+                end, { expr = true, desc = 'Previous hunk' })
 
                 -- Actions
-                map('n', '<leader>ghs', gs.stage_hunk)
-                map('n', '<leader>ghr', gs.reset_hunk)
-                map('v', '<leader>ghs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
-                map('v', '<leader>ghr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end)
-                map('n', '<leader>ghS', gs.stage_buffer)
-                map('n', '<leader>ghu', gs.undo_stage_hunk)
-                map('n', '<leader>ghR', gs.reset_buffer)
-                map('n', '<leader>ghp', gs.preview_hunk)
-                map('n', '<leader>ghb', function() gs.blame_line { full = true } end)
-                map('n', '<leader>gtb', gs.toggle_current_line_blame)
-                map('n', '<leader>ghd', gs.diffthis)
-                map('n', '<leader>ghD', function() gs.diffthis('~') end)
-                map('n', '<leader>gtd', gs.toggle_deleted)
+                map('n', '<leader>ghs', gs.stage_hunk, { desc = 'Stage hunk' })
+                map('n', '<leader>ghr', gs.reset_hunk, { desc = 'Reset hunk' })
+                map('v', '<leader>ghs', function() gs.stage_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
+                    { desc = 'Stage hunk on visual mode' })
+                map('v', '<leader>ghr', function() gs.reset_hunk { vim.fn.line('.'), vim.fn.line('v') } end,
+                    { desc = 'Reset hunk on visual mode' })
+                map('n', '<leader>ghS', gs.stage_buffer, { desc = 'Stage buffer' })
+                map('n', '<leader>ghu', gs.undo_stage_hunk, { desc = 'Undo stage hunk' })
+                map('n', '<leader>ghR', gs.reset_buffer, { desc = 'Reset buffer' })
+                map('n', '<leader>ghp', gs.preview_hunk, { desc = 'Preview hunk' })
+                map('n', '<leader>ghb', function() gs.blame_line { full = true } end, { desc = 'Blame line' })
+                map('n', '<leader>gtb', gs.toggle_current_line_blame, {desc = 'Toggle current line blame'})
+                map('n', '<leader>ghd', gs.diffthis, { desc = 'Diff this' })
+                map('n', '<leader>ghD', function() gs.diffthis('~') end, { desc = 'Diff this (ignore whitespace)' })
+                map('n', '<leader>gtd', gs.toggle_deleted, { desc = 'Toggle deleted' })
 
                 -- Text object
-                map({ 'o', 'x' }, 'gih', ':<C-U>Gitsigns select_hunk<CR>')
+                map({ 'o', 'x' }, 'gih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'Select hunk' })
             end
         })
     end
