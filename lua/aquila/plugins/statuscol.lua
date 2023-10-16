307d340f1288e304ae82566d6dfc23e04fae089d
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-10-16 11:11:46 +0800

    make better gitsign in statuscol

--- lua/aquila/plugins/statuscol.lua
+++ lua/aquila/plugins/statuscol.lua
@@ -1,33 +1,31 @@
 return {
-    {
-        "luukvbaal/statuscol.nvim",
-        config = function()
-            local builtin = require("statuscol.builtin")
-            require("statuscol").setup({
-                relculright = true,
-                segments = {
-                    {
-                        text = { builtin.foldfunc },
-                        click = "v:lua.ScFa",
-                    },
-                    {
-                        text =  {
-                            " ",
-                        }
-                    },
-                    {
-                        sign = {
-                            name = {
-                                "Diagnostic",
-                            },
-                            auto = true,
-                        },
-                        click = "v:lua.ScSa"
-                    },
-                    -- { text = { "%s" } ,  click = "v:lua.ScSa"         },
-                    { text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+    "luukvbaal/statuscol.nvim",
+    config = function()
+        local builtin = require("statuscol.builtin")
+        require("statuscol").setup({
+            relculright = true,
+            segments = {
+                {
+                    text = { builtin.foldfunc },
+                    click = "v:lua.ScFa",
                 },
-            })
-        end,
-    },
+                {
+                    text = { " " }
+                },
+                {
+                    sign = { name = { "Diagnostic" }, auto = true },
+                    click = "v:lua.ScSa"
+                },
+                {
+                    -- Git-colored bar
+                    sign = {
+                        namespace = { "gitsigns" },
+                        auto = true
+                    },
+                    click = "v:lua.ScSa",
+                },
+                { text = { builtin.lnumfunc }, click = "v:lua.ScLa", },
+            },
+        })
+    end,
 }
