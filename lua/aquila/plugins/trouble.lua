aa97408fa771b005e8c09724b03c6841734bf3cf
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-10-17 16:19:46 +0800

    add keybinding for trouble and set which key instead

--- lua/aquila/plugins/trouble.lua
+++ lua/aquila/plugins/trouble.lua
@@ -4,4 +4,19 @@ return {
     event = "BufReadPre",
     opts = {
     },
+    config = function(_, opts)
+        local trouble = require('trouble')
+
+        require('which-key').register({
+            ["<Leader>x"] = { name = "+diagnostic" }
+        })
+
+        vim.keymap.set("n", "<leader>xx", function() trouble.toggle() end, { desc = "Toggle" })
+        vim.keymap.set("n", "<leader>xw", function() trouble.toggle("workspace_diagnostics") end,
+            { desc = "Workspace diagnostics" })
+        vim.keymap.set("n", "<leader>xd", function() trouble.toggle("document_diagnostics") end,
+            { desc = "Document diagnostics" })
+        vim.keymap.set("n", "<leader>xq", function() trouble.toggle("quickfix") end, { desc = "Quickfix" })
+        vim.keymap.set("n", "<leader>xl", function() trouble.toggle("loclist") end, { desc = "Loclist" })
+    end
 }
