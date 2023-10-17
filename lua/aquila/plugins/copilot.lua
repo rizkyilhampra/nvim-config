314255619866959969bd2137b775ed2b7339d191
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-10-18 01:04:02 +0800

    fix key tab not working cause was initialize at keymap copilot

--- lua/aquila/plugins/copilot.lua
+++ lua/aquila/plugins/copilot.lua
@@ -6,11 +6,13 @@ return {
         require("copilot").setup({
             suggestion = {
                 auto_trigger = true,
-                keymap = {
-                    accept = "<Tab>",
-                    dismiss = "<Esc>",
-                }
-            },
+            }
         })
+
+        local cps = require('copilot.suggestion')
+        if cps.is_visible() then
+            vim.set.keymap("i", "<Tab>", function() cps.accept() end, { noremap = true, silent = true })
+            vim.set.keymap("i", "<Esc>", function() cps.dismiss() end, { noremap = true, silent = true })
+        end
     end,
 }
