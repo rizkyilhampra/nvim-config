b83407070ddb41ad71af271c64e2d33dc59fa132
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-10-19 23:18:20 +0800

    rename file

---
+++ lua/aquila/plugins/tokyonight-theme.lua
@@ -0,0 +1,12 @@
+return {
+    "folke/tokyonight.nvim",
+    priority = 1000,
+    lazy = false,
+    opts = {
+        transparent = true,
+        styles = {
+            sidebars = "transparent",
+            floats = "transparent",
+        }
+    },
+}
