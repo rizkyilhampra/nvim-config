19937ab9518c9abb3fedddc58aaaadd723849aaf
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-10-19 23:17:54 +0800

    add color scheme plugin but commented

---
+++ lua/aquila/plugins/kanagawa.lua
@@ -0,0 +1,9 @@
+return {
+    -- "rebelot/kanagawa.nvim",
+    -- priority = 1000,
+    -- config = function()
+    --     require("kanagawa").setup({
+    --         transparent = true
+    --     })
+    -- end
+}
