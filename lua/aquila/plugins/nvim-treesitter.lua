22dabaa24dbfc79b0775f311902065681a441eda
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-10-19 23:23:47 +0800

    add plugin dependencies on treesitter and change ensure_installed value

--- lua/aquila/plugins/nvim-treesitter.lua
+++ lua/aquila/plugins/nvim-treesitter.lua
@@ -1,5 +1,8 @@
 return {
     "nvim-treesitter/nvim-treesitter",
+    dependencies = {
+        'nvim-treesitter/playground',
+    },
     build = ":TSUpdate",
     event = "VeryLazy",
     config = function()
@@ -7,21 +10,40 @@ return {
 
         configs.setup({
             ensure_installed = {
-                "php",
                 "lua",
                 "vim",
                 "vimdoc",
-                "query",
                 "javascript",
+                "typescript",
+                "php",
                 "html",
                 "css",
                 "bash",
                 "sql"
             },
 
-            sync_install = false,
+            sync_install = true,
             highlight = { enable = true },
             indent = { enable = true },
+
+            playground = {
+                enable = true,
+                disable = {},
+                updatetime = 25, -- Debounced time for highlighting nodes in the playground from source code
+                persist_queries = false, -- Whether the query persists across vim sessions
+                keybindings = {
+                    toggle_query_editor = 'o',
+                    toggle_hl_groups = 'i',
+                    toggle_injected_languages = 't',
+                    toggle_anonymous_nodes = 'a',
+                    toggle_language_display = 'I',
+                    focus_language = 'f',
+                    unfocus_language = 'F',
+                    update = 'R',
+                    goto_node = '<cr>',
+                    show_help = '?',
+                },
+            }
         })
 
         local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
