ed2ed12fee7f6488f3e5ce39f6083a001910c2d3
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-10-22 07:58:47 +0800

    refactor ufo for better folding

--- lua/aquila/plugins/ufo.lua
+++ lua/aquila/plugins/ufo.lua
@@ -1,16 +1,15 @@
 return {
     "kevinhwang91/nvim-ufo",
-    event = "BufReadPost",
+    event = { "BufReadPost", "BufNewFile" },
     dependencies = {
         "kevinhwang91/promise-async",
     },
     opts = {
-        -- provider_selector = function()
-        --     return { "treesitter", "indent" }
-        -- end,
+        close_fold_kinds = {
+            'comment', 'import'
+        },
     },
-
-    init = function()
+    keys = function()
         vim.keymap.set("n", "zR", function()
             require("ufo").openAllFolds()
         end, {
@@ -20,4 +19,69 @@ return {
             require("ufo").closeAllFolds()
         end, { desc = "Close all fold" })
     end,
+    config = function()
+        local function get_comment_folds(bufnr)
+            local comment_folds = {}
+            local line_count = vim.api.nvim_buf_line_count(bufnr)
+            local is_in_comment = false
+            local comment_start = 0
+
+            for i = 0, line_count - 1 do
+                local line = vim.api.nvim_buf_get_lines(bufnr, i, i + 1, false)[1]
+                if not is_in_comment and line:match('^%s*' .. vim.o.commentstring:sub(1, 1)) then
+                    is_in_comment = true
+                    comment_start = i
+                elseif is_in_comment and not line:match('^%s*' .. vim.o.commentstring:sub(1, 1)) then
+                    is_in_comment = false
+                    table.insert(comment_folds, { startLine = comment_start, endLine = i - 1 })
+                end
+            end
+
+            if is_in_comment then
+                table.insert(comment_folds, { startLine = comment_start, endLine = line_count - 1 })
+            end
+
+            return comment_folds
+        end
+
+        local function treesitter_and_comment_folding(bufnr)
+            local comment_folds = get_comment_folds(bufnr)
+            local lsp_folds = require('ufo').getFolds(bufnr, "treesitter")
+            for _, fold in ipairs(comment_folds) do
+                table.insert(lsp_folds, fold)
+            end
+            return lsp_folds
+        end
+
+        local ftMap = {
+            vim = 'indent',
+            git = 'indent',
+            lua = function(bufnr)
+                return treesitter_and_comment_folding(bufnr)
+            end
+        }
+        local function fold_text()
+            local text = vim.treesitter.foldtext()
+
+            local n_lines = vim.v.foldend - vim.v.foldstart
+            local text_lines = " lines"
+
+            if n_lines == 1 then
+                text_lines = " line"
+            end
+
+            table.insert(text, { " - " .. n_lines .. text_lines, { "Folded" } })
+
+            return text
+        end
+
+
+        require('ufo').setup({
+            provider_selector = function(bufnr, filetype, buftype)
+                return ftMap[filetype]
+            end,
+            fold_virt_text_handler = fold_text
+        })
+    end
+
 }
