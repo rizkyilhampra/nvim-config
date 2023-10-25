740139566a2e8fd4d0ef350b334154747540b39e
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-10-25 13:29:42 +0800

    change prettier to prettierd

--- lua/aquila/plugins/conform-formatting.lua
+++ lua/aquila/plugins/conform-formatting.lua
@@ -17,12 +17,12 @@ return {
     },
     opts = {
         formatters_by_ft = {
-            css = { "prettier" },
-            javascript = { "prettier" },
-            javascriptreact = { "prettier" },
+            css = { "prettierd" },
+            javascript = { "prettierd" },
+            javascriptreact = { "prettierd" },
             json = { "prettier" },
-            typescript = { "prettier" },
-            typescriptreact = { "prettier" },
+            typescript = { "prettierd" },
+            typescriptreact = { "prettierd" },
             blade = { 'my_blade' }
         },
         formatters = {
@@ -35,7 +35,7 @@ return {
     init = function()
         vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
     end,
-    config = function(_, opts)
-        require("conform").setup(opts)
-    end
+    -- config = function(_, opts)
+    --     require("conform").setup(opts)
+    -- end
 }
