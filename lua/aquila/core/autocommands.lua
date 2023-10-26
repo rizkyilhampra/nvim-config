3f7950073da2eba09adb21526846da4cfe2e2794
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-10-26 23:15:01 +0800

    add autocommand disable new line comment

--- lua/aquila/core/autocommands.lua
+++ lua/aquila/core/autocommands.lua
@@ -27,3 +27,10 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 --         end
 --     end,
 -- })
+
+vim.api.nvim_create_autocmd("BufEnter", {
+    callback = function()
+        vim.opt.formatoptions:remove { "c", "r", "o" }
+    end,
+    desc = "Disable New Line Comment",
+})
