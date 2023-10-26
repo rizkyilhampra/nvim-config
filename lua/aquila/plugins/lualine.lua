a49521a56831812779e4e8e96079399665d1b855
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-10-27 00:07:03 +0800

    make lualine transparent for tokyonight

--- lua/aquila/plugins/lualine.lua
+++ lua/aquila/plugins/lualine.lua
@@ -2,40 +2,47 @@ return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
     event = "VeryLazy",
-    opts = {
-        options = {
-            -- theme = "catppuccin",
-            theme = "tokyonight",
-            globalstatus = true,
-            disabled_filetypes = {
-                statusline = { "alpha", "neo-tree" }
+    opts = function()
+        local theme = require("lualine.themes.tokyonight")
+        if theme then
+            theme.normal.c.bg = nil
+        else
+            theme = 'auto'
+        end
+        return {
+            options = {
+                theme = theme,
+                globalstatus = true,
+                disabled_filetypes = {
+                    statusline = { "alpha", "neo-tree" }
+                },
+                component_separators = '|',
+                section_separators = { left = '', right = '' },
             },
-            component_separators = '|',
-            section_separators = { left = '', right = '' },
-        },
-        sections = {
-            lualine_a = {
-                { 'mode', separator = { left = '' }, right_padding = 2 },
+            sections = {
+                lualine_a = {
+                    { 'mode', separator = { left = '' }, right_padding = 2 },
+                },
+                lualine_b = { 'filename', 'branch' },
+                lualine_c = { 'fileformat' },
+                lualine_x = {},
+                lualine_y = { 'filetype', 'progress' },
+                lualine_z = {
+                    { 'location', separator = { right = '' }, left_padding = 2 },
+                },
             },
-            lualine_b = { 'filename', 'branch' },
-            lualine_c = { 'fileformat' },
-            lualine_x = {},
-            lualine_y = { 'filetype', 'progress' },
-            lualine_z = {
-                { 'location', separator = { right = '' }, left_padding = 2 },
+            inactive_sections = {
+                lualine_a = { 'filename' },
+                lualine_b = {},
+                lualine_c = {},
+                lualine_x = {},
+                lualine_y = {},
+                lualine_z = { 'location' },
             },
-        },
-        inactive_sections = {
-            lualine_a = { 'filename' },
-            lualine_b = {},
-            lualine_c = {},
-            lualine_x = {},
-            lualine_y = {},
-            lualine_z = { 'location' },
-        },
-        tabline = {},
-        extensions = {},
-    },
+            tabline = {},
+            extensions = {},
+        }
+    end,
     init = function()
         vim.g.lualine_laststatus = vim.o.laststatus
         if vim.fn.argc(-1) > 0 then
