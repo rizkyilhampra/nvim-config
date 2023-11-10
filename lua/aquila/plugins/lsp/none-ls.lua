e92aae6b35bced50fe27cd58990d4d83c17b3080
rizkyilhampra <rizkyilhamp16@gmail.com>
2023-11-11 05:06:31 +0800

    feat(lsp): none ls condition when eslint.rc not found

--- lua/aquila/plugins/lsp/none-ls.lua
+++ lua/aquila/plugins/lsp/none-ls.lua
@@ -5,8 +5,16 @@ return {
         local null_ls = require('null-ls')
 
         local sources = {
-            null_ls.builtins.code_actions.eslint_d,
-            null_ls.builtins.diagnostics.eslint_d
+            null_ls.builtins.code_actions.eslint_d.with({
+                condition = function(utils)
+                    return utils.root_has_file('.eslintrc.js', '.eslintrc.json', '.eslintrc')
+                end
+            }),
+            null_ls.builtins.diagnostics.eslint_d.with({
+                condition = function(utils)
+                    return utils.root_has_file('.eslintrc.js', '.eslintrc.json', '.eslintrc')
+                end
+            })
         }
 
         null_ls.setup({
