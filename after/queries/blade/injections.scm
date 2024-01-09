e1753b1960452f6171c779f68d9468279ac022e4
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-01-09 15:21:29 +0800

    chore: update php injection

--- after/queries/blade/injections.scm
+++ after/queries/blade/injections.scm
@@ -1,7 +1,9 @@
-((php) @injection.content
-    (#set! injection.language "php")
-    (#set! injection.combined))
 ((parameter) @injection.content
-    (#set! injection.language "php")) ; TODO: php_only
+    (#set! injection.language php_only)) 
 ((php_only) @injection.content
-    (#set! injection.language "php")) ; TODO: php_only
+   (#set! injection.combined)
+   (#set! injection.language php_only))
+((text) @injection.content
+   (#not-has-ancestor? @injection.content "envoy")
+   (#set! injection.combined)
+   (#set! injection.language php))
