0d8f52d185c107d6e6dbf873eb41dca1ee54ecdc
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-01-09 15:22:59 +0800

    refactor: neo-tree

--- lua/aquila/plugins/neo-tree.lua
+++ lua/aquila/plugins/neo-tree.lua
@@ -5,25 +5,6 @@ return {
         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
         "MunifTanjim/nui.nvim",
         "nvim-lua/plenary.nvim",
-        -- {
-        --     's1n7ax/nvim-window-picker',
-        --     version = '2.*',
-        --     config = function()
-        --         require 'window-picker'.setup({
-        --             filter_rules = {
-        --                 include_current_win = false,
-        --                 autoselect_one = true,
-        --                 -- filter using buffer options
-        --                 bo = {
-        --                     -- if the file type is one of following, the window will be ignored
-        --                     filetype = { 'neo-tree', "neo-tree-popup", "notify" },
-        --                     -- if the buffer type is one of following, the window will be ignored
-        --                     buftype = { 'terminal', "quickfix" },
-        --                 },
-        --             },
-        --         })
-        --     end,
-        -- },
     },
     config = function()
         require("neo-tree").setup({
@@ -31,14 +12,10 @@ return {
             window = {
                 width = 35,
             },
-            buffers = {
-                follow_current_file = {
-                    enabled = true
-                }
-            },
             filesystem = {
                 follow_current_file = {
-                    enabled = true
+                    enabled         = true,
+                    leave_dirs_open = true
                 },
                 filtered_items = {
                     hide_dotfiles = false,
@@ -54,5 +31,7 @@ return {
                 },
             },
         })
+
+        vim.cmd([[nnoremap \ :Neotree reveal<cr>]])
     end
 }
