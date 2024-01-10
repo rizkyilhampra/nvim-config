d09bc14d7db49cc62fa3488747114a991814e9c3
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-01-10 12:41:56 +0800

    refactor: lualine

--- lua/aquila/plugins/lualine.lua
+++ lua/aquila/plugins/lualine.lua
@@ -4,12 +4,6 @@ return {
     event = "VeryLazy",
     enabled = true,
     opts = function()
-        local theme = require("lualine.themes.tokyonight")
-        if theme then
-            theme.normal.c.bg = nil
-        else
-            theme = 'auto'
-        end
         return {
             options = {
                 theme = theme,
@@ -21,16 +15,12 @@ return {
                 section_separators = { left = '', right = '' },
             },
             sections = {
-                lualine_a = {
-                    { 'mode', separator = { left = '' }, right_padding = 2 },
-                },
-                lualine_b = { 'filename', 'branch' },
-                lualine_c = { 'fileformat' },
+                lualine_a = { 'mode'},
+                lualine_b = { 'filename','branch' },
+                lualine_c = {},
                 lualine_x = {},
                 lualine_y = { 'filetype', 'progress' },
-                lualine_z = {
-                    { 'location', separator = { right = '' }, left_padding = 2 },
-                },
+                lualine_z = { 'location' },
             },
             inactive_sections = {
                 lualine_a = { 'filename' },
