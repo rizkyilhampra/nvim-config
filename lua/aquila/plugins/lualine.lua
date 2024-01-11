8886ac3c5f20067cf039d48b615513e04bf20915
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-01-11 22:51:57 +0800

    feat: add lsp-progress.nvim and integration with lualine
    also replace fidget

--- lua/aquila/plugins/lualine.lua
+++ lua/aquila/plugins/lualine.lua
@@ -1,40 +1,45 @@
 return {
     'nvim-lualine/lualine.nvim',
     dependencies = { 'nvim-tree/nvim-web-devicons' },
-    event = "VeryLazy",
+    event = "UIEnter",
     enabled = true,
-    opts = function()
-        return {
-            options = {
-                theme = theme,
-                globalstatus = true,
-                disabled_filetypes = {
-                    statusline = { "alpha", "neo-tree" }
+    opts = {
+        options = {
+            globalstatus = true,
+            disabled_filetypes = {
+                statusline = { "alpha", "neo-tree" }
+            },
+            component_separators = '|',
+            -- section_separators = { left = '', right = '' },
+            section_separators = { left = '', right = '' },
+        },
+        sections = {
+            lualine_a = { 'mode' },
+            lualine_b = { 'filename' },
+            lualine_c = { 'branch' },
+            lualine_x = {
+                {
+                    require('lsp-progress').progress,
+                    icon = { "", align = "right" },
                 },
-                component_separators = '|',
-                -- section_separators = { left = '', right = '' },
-                section_separators = { left = '', right = '' },
+                "diagnostics",
+                'filetype'
+
             },
-            sections = {
-                lualine_a = { 'mode' },
-                lualine_b = { 'filename' },
-                lualine_c = { 'branch' },
-                lualine_x = { 'filetype' },
-                lualine_y = { 'progress' },
-                lualine_z = { 'location' },
-            },
-            inactive_sections = {
-                lualine_a = { 'filename' },
-                lualine_b = {},
-                lualine_c = {},
-                lualine_x = {},
-                lualine_y = {},
-                lualine_z = { 'location' },
-            },
-            tabline = {},
-            extensions = {},
-        }
-    end,
+            lualine_y = { 'progress' },
+            lualine_z = { 'location' },
+        },
+        inactive_sections = {
+            lualine_a = { 'filename' },
+            lualine_b = {},
+            lualine_c = {},
+            lualine_x = {},
+            lualine_y = {},
+            lualine_z = { 'location' },
+        },
+        tabline = {},
+        extensions = {},
+    },
     init = function()
         vim.g.lualine_laststatus = vim.o.laststatus
         if vim.fn.argc(-1) > 0 then
