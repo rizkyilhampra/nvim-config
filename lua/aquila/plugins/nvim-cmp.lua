fc8d512494fdda43c854e63f251be911ca8ebc54
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-01-15 19:10:57 +0800

    chore: some refactor and fixed nvim cmp

--- lua/aquila/plugins/nvim-cmp.lua
+++ lua/aquila/plugins/nvim-cmp.lua
@@ -2,55 +2,26 @@ return {
     "hrsh7th/nvim-cmp",
     event = "InsertEnter",
     dependencies = {
-        "hrsh7th/cmp-buffer",       -- source for text in buffer
-        "hrsh7th/cmp-path",         -- source for file system paths
-        "saadparwaiz1/cmp_luasnip", -- for autocompletion
-        {
-            "rafamadriz/friendly-snippets",
-            config = function()
-                require("luasnip.loaders.from_vscode").lazy_load()
-            end,
-        },
-        {
-            "L3MON4D3/LuaSnip", -- snippet engine
-            opts = {
-                history = true,
-                delete_check_events = "TextChanged",
-            },
-            keys = {
-                {
-                    "<tab>",
-                    function()
-                        return require("luasnip").jumpable(1) and "<Plug>luasnip-jump-next" or "<tab>"
-                    end,
-                    expr = true,
-                    silent = true,
-                    mode = "i",
-                },
-                { "<tab>",   function() require("luasnip").jump(1) end,  mode = "s" },
-                { "<s-tab>", function() require("luasnip").jump(-1) end, mode = { "i", "s" } },
-            }
-        },
-        "onsails/lspkind.nvim", -- vs-code like pictograms
+        "hrsh7th/cmp-buffer",
+        "hrsh7th/cmp-path",
+        "onsails/lspkind.nvim",
+        "L3MON4D3/LuaSnip",
+        "saadparwaiz1/cmp_luasnip",
+        "rafamadriz/friendly-snippets",
     },
     config = function()
-        -- for supertab mapping
-        -- nvim cmp recomndation
-        -- local has_words_before = function()
-        --     unpack = unpack or table.unpack
-        --     local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-        --     return col ~= 0 and vim.api.nvim_buf_get_lines(0, line - 1, line, true)[1]:sub(col, col):match("%s") == nil
-        -- end
-        -- copilot cmp recomendation
-        local has_words_before = function()
-            if vim.api.nvim_buf_get_option(0, "buftype") == "prompt" then return false end
-            local line, col = unpack(vim.api.nvim_win_get_cursor(0))
-            return col ~= 0 and vim.api.nvim_buf_get_text(0, line - 1, 0, line - 1, col, {})[1]:match("^%s*$") == nil
-        end
+        require("luasnip.loaders.from_vscode").lazy_load()
 
         local cmp = require("cmp")
+        local compare = cmp.config.compare
         local luasnip = require("luasnip")
-        local lspkind = require("lspkind")
+        luasnip.config.setup({
+            region_check_events = "CursorMoved",
+            delete_check_events = "TextChanged",
+        })
+
+        require("lspkind")
+        local cps = require('copilot.suggestion')
 
         local cmp_kinds = {
             Text = '  ',
@@ -87,64 +58,89 @@ return {
             },
             completion = {
                 completeopt = "menu,menuone,preview,noselect",
+                keyword_length = 3
             },
-            snippet = { -- configure how nvim-cmp interacts with snippet engine
+            snippet = {
                 expand = function(args)
                     luasnip.lsp_expand(args.body)
                 end,
             },
+            --old
+            -- window = {
+            --     -- completion = cmp.config.window.bordered(),
+            --     -- completion = {
+            --     --     -- border = "rounded",
+            --     --     -- winhighlight = "Normal:TelescopeNormal",
+            --     -- },
+            --
+            --     documentation = cmp.config.window.bordered(),
+            --     -- documentation = {
+            --     --     border = "rounded",
+            --     --     winhighlight = 'normal:pmenu,floatborder:pmenu',
+            --     -- },
+            -- },
             window = {
-                -- completion = cmp.config.window.bordered(),
-                -- completion = {
-                --     -- border = "rounded",
-                --     -- winhighlight = "Normal:TelescopeNormal",
-                -- },
-
-                documentation = cmp.config.window.bordered(),
-                -- documentation = {
-                --     border = "rounded",
-                --     winhighlight = 'normal:pmenu,floatborder:pmenu',
-                -- },
+                documentation = {
+                    maxheight = 15,
+                    maxwidth = 50,
+                },
+                completion = {
+                    -- col_offset = -3,
+                    -- side_padding = 0,
+                    winhighlight = "Normal:Pmenu,FloatBorder:FloatBorder,CursorLine:Visual,Search:None",
+                },
             },
+            enabled = function()
+                if require "cmp.config.context".in_treesitter_capture("comment") == true or require "cmp.config.context".in_syntax_group("Comment") then
+                    return false
+                else
+                    return true
+                end
+            end,
             mapping = cmp.mapping.preset.insert({
-                -- ["<C-k>"] = cmp.mapping.select_prev_item(), -- previous suggestion
-                -- ["<C-j>"] = cmp.mapping.select_next_item(), -- next suggestion
                 ["<C-b>"] = cmp.mapping.scroll_docs(-4),
                 ["<C-f>"] = cmp.mapping.scroll_docs(4),
                 ["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
-                ["<C-e>"] = cmp.mapping.abort(),        -- close completion window
                 ["<CR>"] = cmp.mapping.confirm({ select = false }),
-                -- supertab mapping
-                ["<Tab>"] = cmp.mapping(function(fallback)
-                    local cp = require('copilot.suggestion')
-
+                ["<Esc>"] = cmp.mapping.abort(),
+                ['<Tab>'] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_next_item()
-                    elseif cp.is_visible() then
-                        cp.accept()
-                    elseif has_words_before() then
-                        cmp.complete()
+                    elseif cps.is_visible() then
+                        cps.accept()
+                    elseif luasnip.expandable() then
+                        luasnip.expand()
+                    elseif luasnip.jumpable(1) then
+                        luasnip.jump(1)
                     else
                         fallback()
                     end
-                end, { "i", "s" }),
-                ["<S-Tab>"] = cmp.mapping(function(fallback)
+                end, { 'i', 's' }),
+                ['<S-Tab>'] = cmp.mapping(function(fallback)
                     if cmp.visible() then
                         cmp.select_prev_item()
+                    elseif luasnip.jumpable(-1) then
+                        luasnip.jump(-1)
                     else
                         fallback()
                     end
-                end, { "i", "s" }),
-                -- endsupertab mapping
+                end, { 'i', 's' }),
             }),
-            -- sources for autocompletion
             sources = cmp.config.sources({
                 { name = "nvim_lsp", group_index = 1 },
-                { name = "luasnip",  group_index = 3 }, -- snippets
-                { name = "buffer",   group_index = 2 }, -- text within current buffer
-                { name = "path",     group_index = 2 }, -- file system paths
+                { name = "luasnip",  group_index = 3 },
+                { name = "path",     keyword_length = 1, group_index = 4 },
+                { name = "buffer",   group_index = 2 },
             }),
-            -- configure lspkind for vs-code like pictograms in completion menu
+            sorting = {
+                comparators = {
+                    compare.locality,
+                    compare.recently_used,
+                    compare.score,
+                    compare.offset,
+                    compare.order
+                }
+            },
             formatting = {
                 fields = { "kind", "abbr" },
                 format = function(_, vim_item)
@@ -153,13 +149,5 @@ return {
                 end,
             },
         })
-
-        cmp.event:on("menu_opened", function(fallback)
-            vim.b.copilot_suggestion_hidden = true
-        end)
-
-        cmp.event:on("menu_closed", function(fallback)
-            vim.b.copilot_suggestion_hidden = false
-        end)
     end,
 }
