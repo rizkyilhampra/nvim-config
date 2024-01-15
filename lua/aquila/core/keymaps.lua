aa6bd738707563eb859c4f12574725d7f69f5a98
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-01-15 19:06:54 +0800

    chore: delete keymap switch buffer and change keymap to go alpha dash

--- lua/aquila/core/keymaps.lua
+++ lua/aquila/core/keymaps.lua
@@ -6,9 +6,6 @@ set("i", "jj", "<Esc>", { noremap = true, silent = true }) -- the important thin
 
 set({ 'i', 'n' }, "<C-s>", '<cmd>w<CR>')                   -- i love vscode and other editor at windows behavior for save a file
 
-set('n', '<C-h>', '<cmd>bnext<CR>')                        -- switch buffer next or previous
-set('n', '<C-l>', '<cmd>bprevious<CR>')
-
 set({ 'n', 'v', 'i' }, '<C-z>', '<Nop>') -- fix problem when CTRL + Z is exit neovim
 set('n', '<C-c>', '<Nop>')               -- fix probelm when CTRL + C in normal mode is exit neovim
 
@@ -27,7 +24,7 @@ set({ "n", "v" }, "<Leader>y", "\"+y", { desc = "Copy/Yank to system clipboard" 
 set({ "n", "v" }, "<Leader>p", "\"+p", { desc = "Paste from system clipboard" })
 set("v", "<leader>P", '"_dP', { desc = 'Do not lose the " register on paste' })
 
-set("n", "<Leader>h", '<cmd>Alpha<CR>', { desc = "Take me home to the place i belong ~~" })
+set("n", "<Leader>~", '<cmd>Alpha<CR>', { desc = "Take me home to the place i belong ~~" })
 
 set("n", "H", "<C-w>h", { noremap = true, silent = true })
 set("n", "L", "<C-w>l", { noremap = true, silent = true })
@@ -35,6 +32,3 @@ set("n", "L", "<C-w>l", { noremap = true, silent = true })
 -- Neotree
 set("n", "<Leader>e", '<cmd>Neotree toggle<CR>')
 set("n", "<Leader>E", '<cmd>Neotree reveal<CR>')
-
--- remap <Esc> to <C-\><C-n> to exit terminal mode
--- vim.cmd([[tnoremap <Esc> <C-\><C-n>]]) 
