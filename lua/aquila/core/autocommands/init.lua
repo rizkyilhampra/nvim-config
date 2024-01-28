7e7415154adb5431e5710a8058342468ea32c9b6
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-01-28 17:26:24 +0800

    chore(Autocommand): refactor and also add auto save cmd

--- lua/aquila/core/autocommands/init.lua
+++ lua/aquila/core/autocommands/init.lua
@@ -34,3 +34,29 @@ vim.api.nvim_create_autocmd("BufEnter", {
     end,
     desc = "Disable New Line Comment",
 })
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        if mark[1] > 1 and mark[1] <= vim.api.nvim_buf_line_count(0) then
+            vim.api.nvim_win_set_cursor(0, mark)
+        end
+    end,
+    desc = "Restore cursor position",
+})
+
+vim.api.nvim_create_autocmd("BufReadPost", {
+    callback = function()
+        local home = os.getenv("HOME")
+        if not vim.fn.isdirectory(home .. "/.vim") then
+            vim.fn.mkdir(home .. "/.vim", "", "rwxrw-")
+        end
+        if not vim.fn.isdirectory(home .. "/.vim/undo-dir") then
+            vim.fn.mkdir(home .. "/.vim/undo-dir", "", "rwx")
+        end
+    end,
+    desc = "Create undo-dir",
+})
+
+--automatically save the current buffer (file) under certain conditions.
+require('aquila.core.autocommands.auto-save')
