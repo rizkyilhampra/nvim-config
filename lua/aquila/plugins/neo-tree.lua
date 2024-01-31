227dde8e5530d5106aece54a5f8d919dbf3178fc
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-01-31 18:42:08 +0800

    feat(Neotree): add mapping for better navigation with H and L

--- lua/aquila/plugins/neo-tree.lua
+++ lua/aquila/plugins/neo-tree.lua
@@ -12,6 +12,27 @@ return {
             use_libuv_file_watcher = true,
             window = {
                 width = 35,
+                mappings = {
+                    ["h"] = function(state)
+                        local node = state.tree:get_node()
+                        if node.type == 'directory' and node:is_expanded() then
+                            require 'neo-tree.sources.filesystem'.toggle_directory(state, node)
+                        else
+                            require 'neo-tree.ui.renderer'.focus_node(state, node:get_parent_id())
+                        end
+                    end,
+                    ["l"] = function(state)
+                        local node = state.tree:get_node()
+                        if node.type == 'directory' then
+                            if not node:is_expanded() then
+                                require 'neo-tree.sources.filesystem'.toggle_directory(state, node)
+                            elseif node:has_children() then
+                                require 'neo-tree.ui.renderer'.focus_node(state, node:get_child_ids()[1])
+                            end
+                        elseif node.type == 'file' then
+                            require("neo-tree.sources.filesystem.commands").open(state)
+                        end
+                    end,
                     -- copy to system clipboard
                     ["Y"] = function(state)
                         local currentNode = state.tree:get_node()
@@ -125,12 +146,5 @@ return {
                 }
             },
         })
-
-        -- @todo
-        -- require("neo-tree.sources.filesystem.commands")
-        --     .refresh(
-        --         require("neo-tree.sources.manager")
-        --         .get_state("filesystem")
-        --     )
     end
 }
