e2b332f71bb39fca57e0c1bdd32de0d3b3a109d0
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-02-02 15:30:09 +0800

    chore(Neotree): handle error if delete to trash not working

--- lua/aquila/plugins/neo-tree.lua
+++ lua/aquila/plugins/neo-tree.lua
@@ -118,36 +118,62 @@ return {
                         inputs.confirm(msg, function(confirmed)
                             if not confirmed then return end
 
-                            vim.fn.system { "trash", vim.fn.fnameescape(path) }
+                            local success, error = pcall(function()
+                                vim.fn.system { "trash", vim.fn.fnameescape(path) }
+                            end)
+
+                            if not success then
+                                local errorNotification = {
+                                    title = "NeoTree",
+                                    timeout = 1000,
+                                }
+                                vim.notify("Failed to delete to trash", "error", errorNotification)
+
+                                msg = "Skip trash? (permanent delete)"
+
+                                inputs.confirm(msg, function(confirmed)
+                                    if not confirmed then return end
+
+                                    if state.tree:get_node().type == "directory" then
+                                        vim.fn.system { "rm", "-rf", vim.fn.fnameescape(path) }
+                                    else
+                                        vim.fn.system { "rm", vim.fn.fnameescape(path) }
+                                    end
+                                end)
+                            end
                             require("neo-tree.sources.manager").refresh(state.name)
                         end)
                     end,
 
+                    -- currently i'm not using this, but it's a good example of how to override default commands
                     -- over write default 'delete_visual' command to 'trash' x n.
-                    delete_visual = function(state, selected_nodes)
-                        local inputs = require("neo-tree.ui.inputs")
-
-                        -- get table items count
-                        function GetTableLen(tbl)
-                            local len = 0
-                            for n in pairs(tbl) do
-                                len = len + 1
-                            end
-                            return len
-                        end
-
-                        local count = GetTableLen(selected_nodes)
-                        local msg = "Are you sure you want to trash " .. count .. " files ?"
-                        inputs.confirm(msg, function(confirmed)
-                            if not confirmed then return end
-                            for _, node in ipairs(selected_nodes) do
-                                vim.fn.system { "trash", vim.fn.fnameescape(node.path) }
-                            end
-                            require("neo-tree.sources.manager").refresh(state.name)
-                        end)
-                    end,
+                    -- delete_visual = function(state, selected_nodes)
+                    --     local inputs = require("neo-tree.ui.inputs")
+                    --
+                    --     -- get table items count
+                    --     function GetTableLen(tbl)
+                    --         local len = 0
+                    --         for n in pairs(tbl) do
+                    --             len = len + 1
+                    --         end
+                    --         return len
+                    --     end
+                    --
+                    --     local count = GetTableLen(selected_nodes)
+                    --     local msg = "Are you sure you want to trash " .. count .. " files ?"
+                    --     inputs.confirm(msg, function(confirmed)
+                    --         if not confirmed then return end
+                    --         for _, node in ipairs(selected_nodes) do
+                    --             vim.fn.system { "trash", vim.fn.fnameescape(node.path) }
+                    --         end
+                    --         require("neo-tree.sources.manager").refresh(state.name)
+                    --     end)
+                    -- end,
                 }
             },
         })
+
+        vim.keymap.set("n", "<Leader>e", '<cmd>Neotree toggle<CR>')
+        vim.keymap.set("n", "<Leader>E", '<cmd>Neotree reveal<CR>')
     end
 }
