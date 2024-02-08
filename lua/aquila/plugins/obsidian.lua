8ba9ecc430009d965e050ca0c4acfb58de22af41
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-02-08 16:13:46 +0800

    feat: add obsidian plugin

---
+++ lua/aquila/plugins/obsidian.lua
@@ -0,0 +1,28 @@
+return {
+    "epwalsh/obsidian.nvim",
+    version = "*",
+    event = {
+        "BufReadPre",
+        "BufNewFile",
+    },
+    dependencies = {
+        "nvim-lua/plenary.nvim",
+    },
+    opts = {
+        workspaces = {
+            {
+                name = "aquila",
+                path = "~/Notes/aquila-notes/personal",
+            },
+            {
+                name = "personal-blog",
+                path = "~/Projects/obsidian-blog/resources/markdown/articles"
+            }
+        },
+    },
+
+    config = function (_, opts)
+        require('obsidian').setup(opts)
+        vim.opt_local.conceallevel = 2
+    end
+}
