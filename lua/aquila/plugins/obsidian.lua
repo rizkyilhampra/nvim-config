be26ad6e6ffb43097d9ba11b9f05a886a65145b3
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-02-19 06:01:10 +0800

    chore(obsidian.nvim): change event and refactor

--- lua/aquila/plugins/obsidian.lua
+++ lua/aquila/plugins/obsidian.lua
@@ -4,22 +4,17 @@ return {
     dependencies = {
         "nvim-lua/plenary.nvim",
     },
-    lazy = true,
-    ft = "markdown",
+    event = "VeryLazy",
     opts = {
         workspaces = {
             {
                 name = "aquila",
-                path = "~/Notes/aquila-notes/personal",
+                path = "~/Notes/aquila-notes/",
             },
             {
                 name = "personal-blog",
                 path = "~/Projects/obsidian-blog/resources/markdown/articles"
             }
         },
-    },
-
-    config = function(_, opts)
-        require('obsidian').setup(opts)
-    end
+    }
 }
