33a8a8bb86fff76db5795655427de0e2ad3ccd8b
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-02-22 19:23:32 +0800

    chore(visual-multi): add init for mapping

--- lua/aquila/plugins/visual-multi.lua
+++ lua/aquila/plugins/visual-multi.lua
@@ -2,4 +2,10 @@ return {
     'mg979/vim-visual-multi',
     branch = 'master',
     event = { "BufReadPre", "BufNewFile" }, -- to disable, comment this out
+    init = function()
+        vim.g.VM_maps = {
+            ["Visual All"] = '<M-d>',
+            ["Select All"] = '<M-d>'
+        }
+    end,
 }
