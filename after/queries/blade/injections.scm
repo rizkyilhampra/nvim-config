38a5c5950c67014cdd81a5ff95c235353b382f32
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-03-02 22:23:34 +0800

    chore(blade treesitter)!: following https://github.com/calebdw/dotfiles

--- after/queries/blade/injections.scm
+++
@@ -1,9 +0,0 @@
-((parameter) @injection.content
-    (#set! injection.language php_only)) 
-((php_only) @injection.content
-   (#set! injection.combined)
-   (#set! injection.language php_only))
-((text) @injection.content
-   (#not-has-ancestor? @injection.content "envoy")
-   (#set! injection.combined)
-   (#set! injection.language php))
---
+++ queries/blade/highlights.scm
@@ -0,0 +1,6 @@
+(directive) @tag
+(directive_start) @tag
+(directive_end) @tag
+(bracket_start) @punctuation.special
+(bracket_end) @punctuation.special
+(comment) @comment @spell
