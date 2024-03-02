0e678bf5783b4c31ab29da7ca811dc5387539568
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-03-02 22:24:37 +0800

    chore(treesitter): refactor, add php_only, and add grammar for phpdoc

--- lua/aquila/plugins/nvim-treesitter.lua
+++ lua/aquila/plugins/nvim-treesitter.lua
@@ -7,10 +7,36 @@ return {
         "nvim-treesitter/nvim-treesitter-textobjects",
     },
     config = function()
+        local parser_config = require('nvim-treesitter.parsers').get_parser_configs()
+        parser_config.blade = {
+            install_info = {
+                url = "https://github.com/EmranMR/tree-sitter-blade",
+                files = { "src/parser.c" },
+                branch = "main",
+            },
+            generate_requires_npm = true,
+            requires_generate_from_grammar = true,
+            filetype = "blade"
+        }
+        parser_config.phpdoc = {
+            install_info = {
+                url = '~/sources/treesitter/tree-sitter-phpdoc',
+                files = {
+                    'src/parser.c',
+                    'src/scanner.c',
+                },
+                branch = "master",
+                generate_requires_npm = true,
+                requires_generate_from_grammar = true,
+            },
+            filetype = 'php',
+        }
+
         require("nvim-treesitter.configs").setup({
             ensure_installed = {
                 "lua", "vim", "vimdoc", "javascript", "typescript", "php", "html",
                 "css", "bash", "sql", "regex", "markdown_inline", "markdown",
+                "php_only"
             },
             sync_install = true,
             auto_install = true,
@@ -20,24 +46,14 @@ return {
             modules = {}
         })
 
-        local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
-        parser_config.blade = {
-            install_info = {
-                url = "https://github.com/EmranMR/tree-sitter-blade",
-                files = { "src/parser.c" },
-                branch = "main",
-            },
-            filetype = "blade"
-        }
-
-        local bladeGrp
-        vim.api.nvim_create_augroup("BladeFiltypeRelated", { clear = true })
-        vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
-            pattern = "*.blade.php",
-            group = bladeGrp,
-            callback = function()
-                vim.opt.filetype = "blade"
-            end,
-        })
+        -- local bladeGrp
+        -- vim.api.nvim_create_augroup("BladeFiltypeRelated", { clear = true })
+        -- vim.api.nvim_create_autocmd({ "BufNewFile", "BufRead" }, {
+        --     pattern = "*.blade.php",
+        --     group = bladeGrp,
+        --     callback = function()
+        --         vim.opt.filetype = "blade"
+        --     end,
+        -- })
     end
 }
