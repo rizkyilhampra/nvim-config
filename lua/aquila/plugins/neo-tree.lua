6c8fa0ded8f0c787155fcd82600b32d9439bf3f2
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-03-04 05:59:56 +0800

    chore(neo-tree): add icon for diagnostics

--- lua/aquila/plugins/neo-tree.lua
+++ lua/aquila/plugins/neo-tree.lua
@@ -10,6 +10,16 @@ return {
         "nvim-lua/plenary.nvim",
     },
     config = function()
+        -- todo: remove this if tag not 3.14 
+        vim.fn.sign_define("DiagnosticSignError",
+            { text = " ", texthl = "DiagnosticSignError" })
+        vim.fn.sign_define("DiagnosticSignWarn",
+            { text = " ", texthl = "DiagnosticSignWarn" })
+        vim.fn.sign_define("DiagnosticSignInfo",
+            { text = " ", texthl = "DiagnosticSignInfo" })
+        vim.fn.sign_define("DiagnosticSignHint",
+            { text = "󰌵", texthl = "DiagnosticSignHint" })
+
         require("neo-tree").setup({
             close_if_last_window = true,
             window = {
