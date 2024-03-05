85cc45ebdb921483abf5e9295c7e1994c787a3af
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-03-05 12:32:53 +0800

    chore(neo-tree): update

--- lua/aquila/plugins/neo-tree.lua
+++ lua/aquila/plugins/neo-tree.lua
@@ -3,23 +3,12 @@ local global = require('aquila.core.global')
 return {
     "nvim-neo-tree/neo-tree.nvim",
     branch = "v3.x",
-    tag = "3.14",
     dependencies = {
         "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
         "MunifTanjim/nui.nvim",
         "nvim-lua/plenary.nvim",
     },
     config = function()
-        -- todo: remove this if tag not 3.14 
-        vim.fn.sign_define("DiagnosticSignError",
-            { text = " ", texthl = "DiagnosticSignError" })
-        vim.fn.sign_define("DiagnosticSignWarn",
-            { text = " ", texthl = "DiagnosticSignWarn" })
-        vim.fn.sign_define("DiagnosticSignInfo",
-            { text = " ", texthl = "DiagnosticSignInfo" })
-        vim.fn.sign_define("DiagnosticSignHint",
-            { text = "󰌵", texthl = "DiagnosticSignHint" })
-
         require("neo-tree").setup({
             close_if_last_window = true,
             window = {
