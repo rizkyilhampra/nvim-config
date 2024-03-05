635e5384be6c581d5a309d7f8897055d82c66ceb
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-03-05 12:35:02 +0800

    refactor: handle breaking changes none-ls (i hate none-ls)

--- lua/aquila/plugins/lsp/none-ls.lua
+++ lua/aquila/plugins/lsp/none-ls.lua
@@ -1,21 +1,29 @@
 return {
     'nvimtools/none-ls.nvim',
-    event = 'BufReadPre',
-    enabled = false,
+    event = {
+        "BufReadPre",
+        "BufWritePost"
+    },
+    dependencies = {
+        "nvimtools/none-ls-extras.nvim",
+    },
     config = function()
         local null_ls = require('null-ls')
 
         local sources = {
-            null_ls.builtins.code_actions.eslint_d.with({
-                condition = function(utils)
-                    return utils.root_has_file('.eslintrc.js', '.eslintrc.cjs', '.eslintrc.json', '.eslintrc')
-                end
+            require("none-ls.diagnostics.eslint").with({
+                name = "eslint_d",
+                meta = {
+                    url = "https://github.com/mantoni/eslint_d.js/",
+                    description = "Like ESLint, but faster.",
+                    notes = {
+                        "Once spawned, the server will continue to run in the background. This is normal and not related to null-ls. You can stop it by running `eslint_d stop` from the command line.",
+                    },
+                },
+                command = "eslint_d",
             }),
-            null_ls.builtins.diagnostics.eslint_d.with({
-                condition = function(utils)
-                    return utils.root_has_file('.eslintrc.js', '.eslintrc.cjs', '.eslintrc.json', '.eslintrc')
-                end
-            })
+            null_ls.builtins.diagnostics.selene,
+            -- null_ls.builtins.diagnostics.phpstan,
         }
 
         null_ls.setup({
