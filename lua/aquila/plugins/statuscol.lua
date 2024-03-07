9605c65f4f79c872e9c4988f4cc62274bb3d7aa9
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-03-07 19:19:45 +0800

    fix(statuscol): fold icon not complete rendering

--- lua/aquila/plugins/statuscol.lua
+++ lua/aquila/plugins/statuscol.lua
@@ -1,15 +1,15 @@
 return {
     "luukvbaal/statuscol.nvim",
-    event = "BufReadPre",
+    event = "BufEnter",
     config = function()
         local builtin = require("statuscol.builtin")
         require("statuscol").setup({
             segments = {
                 relculright = true,
-                { sign = { namespace = { "diagnostic*" }, auto = true },     click = "v:lua.ScSa" },
-                { sign = { namespace = { "gitsigns" }, auto = true, }, click = "v:lua.ScSa", },
-                { text = { builtin.lnumfunc, " " },                    click = "v:lua.ScLa", },
-                { text = { builtin.foldfunc, "  " },                    click = "v:lua.ScFa", },
+                { sign = { namespace = { "diagnostic*" }, auto = true }, click = "v:lua.ScSa" },
+                { sign = { namespace = { "gitsigns" }, auto = true, },   click = "v:lua.ScSa", },
+                { text = { builtin.lnumfunc, " " },                      click = "v:lua.ScLa", },
+                { text = { builtin.foldfunc, "  " },                     click = "v:lua.ScFa", },
             },
         })
     end,
