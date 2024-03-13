5de6104e4cbcd86d9ccfb9d70a9dc4002bb4b534
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-03-14 03:13:24 +0800

    refactor: autocommands now return as module

---
+++ lua/aquila/core/autocommands.lua
@@ -0,0 +1,79 @@
+local M = {}
+
+-- A function to create a function that creates autocmds for an augroup
+function M.create_wrapper(augroup_name)
+    local augroup = vim.api.nvim_create_augroup(augroup_name, { clear = true })
+    return function(event, opts)
+        opts.group = opts.group or augroup
+        return vim.api.nvim_create_autocmd(event, opts)
+    end, augroup
+end
+
+M.create, M.aquila_augroup = M.create_wrapper('aquila')
+
+M.create({ "BufWinLeave" }, {
+    pattern = "*.*",
+    desc = "Remember folds",
+    callback = function()
+        vim.cmd("mkview")
+    end,
+})
+
+M.create({ "BufWinEnter" }, {
+    pattern = "*.*",
+    desc = "Restore folds",
+    callback = function()
+        vim.cmd("silent! loadview")
+    end,
+})
+
+M.create("TextYankPost", {
+    callback = function()
+        vim.highlight.on_yank()
+    end,
+    desc = "Highlight yank",
+})
+
+M.create("BufEnter", {
+    callback = function()
+        vim.opt.formatoptions:remove { "c", "r", "o" }
+    end,
+    desc = "Disable New Line Comment",
+})
+
+M.create("BufReadPost", {
+    callback = function()
+        local mark = vim.api.nvim_buf_get_mark(0, '"')
+        if mark[1] > 1 and mark[1] <= vim.api.nvim_buf_line_count(0) then
+            vim.api.nvim_win_set_cursor(0, mark)
+        end
+    end,
+    desc = "Restore cursor position",
+})
+
+M.create("BufReadPost", {
+    callback = function()
+        local home = os.getenv("HOME")
+        if not vim.fn.isdirectory(home .. "/.vim") then
+            vim.fn.mkdir(home .. "/.vim", "", "rwxrw-")
+        end
+        if not vim.fn.isdirectory(home .. "/.vim/undo-dir") then
+            vim.fn.mkdir(home .. "/.vim/undo-dir", "", "rwx")
+        end
+    end,
+    desc = "Create undo-dir",
+})
+
+
+-- i'm not sure with this
+-- vim.api.nvim_create_autocmd("BufEnter", {
+--     callback = function()
+--         if vim.opt.foldmethod:get() == "expr" then
+--             vim.schedule(function()
+--                 vim.opt.foldmethod = "expr"
+--             end)
+--         end
+--     end,
+-- })
+
+return M
