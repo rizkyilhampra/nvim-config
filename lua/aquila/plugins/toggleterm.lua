2dea83c73ddce7b38fb988200caaf3e5ad512296
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-03-16 04:15:29 +0800

    chore(toggleterm): some refactor and added list

--- lua/aquila/plugins/toggleterm.lua
+++ lua/aquila/plugins/toggleterm.lua
@@ -1,52 +1,88 @@
+function _G.set_terminal_keymaps()
+    local opts = { buffer = 0 }
+    vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+    vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+    vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+    vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+    vim.keymap.set('n', 'q', '<cmd>close<CR>', opts)
+    vim.keymap.set('t', '<Esc><Esc>', '<cmd>close<CR>', opts)
+    vim.keymap.set("n", "<C-t>", '<cmd>exe v:count1 . "ToggleTerm"<CR>',
+        opts)
+end
+
 return {
     'akinsho/toggleterm.nvim',
     version = "*",
-    opts = { --[[ things you want to change go here]] },
     event = "VeryLazy",
-    config = function()
-        local Terminal = require('toggleterm.terminal').Terminal
+    opts = {
+        highlights = {
+            FloatBorder = {
+                link = "FloatBorder"
+            }
+        },
+        float_opts = {
+            border = "curved"
+        }
+    },
+    config = function(_, opts)
+        require('toggleterm').setup(opts)
 
-        function _G.set_terminal_keymaps()
-            local opts = { buffer = 0 }
-            -- vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
-            -- vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
-            -- vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
-            -- vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
-            vim.keymap.set('n', 'q', '<cmd>close<CR>', opts)
-            vim.keymap.set('t', '<Esc><Esc>', '<cmd>close<CR>', opts)
-        end
+        local Terminal = require('toggleterm.terminal').Terminal
 
         vim.cmd("autocmd! TermOpen term://* lua set_terminal_keymaps()")
 
-        local cwd_term  = Terminal:new({
+        local escape_keymap   = function(term)
+            vim.api.nvim_buf_set_keymap(term.bufnr, "t", "jj", [[<C-\><C-n>]], { noremap = true, silent = true })
+        end
+
+        local close_keymap    = function(term)
+            vim.api.nvim_buf_set_keymap(term.bufnr, "t", "q", [[<C-\><C-n>:q<CR>]], { noremap = true, silent = true })
+        end
+
+        local cwd_term        = Terminal:new({
+            direction = "float",
+            on_open = function(term)
+                vim.cmd("startinsert!")
+                escape_keymap(term)
+            end,
+        })
+
+        local horizontal_term = Terminal:new({
+            direction = "horizontal",
+            on_open = function(term)
+                vim.cmd("startinsert!")
+                escape_keymap(term)
+                vim.opt_local.spell = false
+            end,
+        })
+
+        local lazydocker      = Terminal:new({
+            cmd = "lazydocker",
             direction = "float",
             float_opts = {
-                -- winblend = 10
+                height = 50,
+                width  = 150,
             },
             on_open = function(term)
                 vim.cmd("startinsert!")
-                -- vim.api.nvim_buf_set_keymap(term.bufnr, 'n', 'q', "<cmd>close<CR>",
-                --     { noremap = true, silent = true })
-                vim.api.nvim_buf_set_keymap(term.bufnr, "t", "jj", [[<C-\><C-n>]], { noremap = true, silent = true })
-            end,
-            -- function to run on closing the terminal
-            on_close = function(term)
+                close_keymap(term)
             end,
         })
 
-        local home_term = Terminal:new({
+        local lazysql         = Terminal:new({
+            cmd = "lazysql",
             direction = "float",
-            dir = "~",
+            float_opts = {
+                height = 50,
+                width  = 150,
+            },
             on_open = function(term)
                 vim.cmd("startinsert!")
-                -- vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
-                vim.api.nvim_buf_set_keymap(term.bufnr, "t", "jj", [[<C-\><C-n>]], { noremap = true, silent = true })
-            end,
-            on_close = function(term)
+                close_keymap(term)
             end,
         })
 
-        local lazygit   = Terminal:new({
+        local lazygit         = Terminal:new({
             cmd = "lazygit",
             dir = "git_dir",
             direction = "float",
@@ -56,18 +92,17 @@ return {
             },
             on_open = function(term)
                 vim.cmd("startinsert!")
-                vim.api.nvim_buf_set_keymap(term.bufnr, "n", "q", "<cmd>close<CR>", { noremap = true, silent = true })
+                close_keymap(term)
             end,
-            on_close = function(term)
+            on_close = function()
                 -- refresh current buffer
                 vim.cmd("e")
 
-                -- is open neotree?
+                -- refresh neo-tree if it's open
                 local manager = require("neo-tree.sources.manager")
                 local renderer = require("neo-tree.ui.renderer")
                 local state = manager.get_state("filesystem")
                 local window_exists = renderer.window_exists(state)
-
                 if window_exists then
                     vim.cmd [[ lua require("neo-tree.sources.filesystem.commands").refresh(require("neo-tree.sources.manager").get_state("filesystem"))]]
                 end
@@ -80,10 +115,18 @@ return {
 
         vim.keymap.set("n", "<Leader>tt", function()
             cwd_term:toggle()
-        end, { desc = "Toggle terminal on cwd" })
+        end, { desc = "Toggle terminal on cwd with floating" })
 
-        vim.keymap.set("n", "<Leader>tT", function()
-            home_term:toggle()
-        end, { desc = "Toggle terminal on ~ directory" })
+        vim.keymap.set("n", "<Leader>tv", function()
+            horizontal_term:toggle()
+        end, { desc = "Toggle terminal on cwd with vertical" })
+
+        vim.keymap.set("n", "<Leader>ts", function()
+            lazysql:toggle()
+        end, { desc = "Toggle terminal as Lazysql" })
+
+        vim.keymap.set("n", "<Leader>td", function()
+            lazydocker:toggle()
+        end, { desc = "Toggle terminal as Lazydocker" })
     end
 }
