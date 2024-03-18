7904641729797a7a7a7e5ad73a2a07d127eb7487
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-03-19 06:54:52 +0800

    feat: add grapple plug

---
+++ lua/aquila/plugins/grapple.lua
@@ -0,0 +1,19 @@
+return {
+    "cbochs/grapple.nvim",
+    dependencies = {
+        { "nvim-tree/nvim-web-devicons", lazy = true }
+    },
+    opts = {
+        scope = "git", -- also try out "git_branch"
+    },
+    event = { "BufReadPost", "BufNewFile" },
+    cmd = "Grapple",
+    keys = {
+        {
+            "<Leader><Tab>", "<cmd>Grapple toggle_tags<CR>", desc = "Grapple toggle tags",
+        },
+        {
+            "<Leader>GG", "<cmd>Grapple toggle<CR>", desc = "Grapple toggle tag"
+        }
+    }
+}
