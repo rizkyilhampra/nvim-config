891a92f72beb07f80fd1228c2576824171b11886
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-03-19 07:13:05 +0800

    refactor(trouble): change event and lazy loaded

--- lua/aquila/plugins/trouble.lua
+++ lua/aquila/plugins/trouble.lua
@@ -1,18 +1,41 @@
 return {
     "folke/trouble.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
-    event = "BufReadPre",
-    opts = {
+    lazy = true,
+    opts = {},
+    keys = {
+        {
+            "<leader>xx",
+            function()
+                require('trouble').toggle()
+            end,
+            desc = "Toggle"
+        },
+        {
+            "<leader>xw",
+            function()
+                require('trouble').toggle("workspace_diagnostics")
+            end,
+            desc = "Workspace diagnostics"
+        },
+        {
+            "<leader>xd",
+            function()
+                require('trouble').toggle("document_diagnostics")
+            end,
+            desc = "Document diagnostics"
+        },
+        {
+            "<leader>xq",
+            function()
+                require('trouble').toggle('quickfix')
+            end,
+            desc = "Quickfix"
+        },
+        {
+            "<leader>xl",
+            function() require('trouble').toggle("loclist") end,
+            desc = "Loclist"
+        }
     },
-    config = function(_, opts)
-        local trouble = require('trouble')
-
-        vim.keymap.set("n", "<leader>xx", function() trouble.toggle() end, { desc = "Toggle" })
-        vim.keymap.set("n", "<leader>xw", function() trouble.toggle("workspace_diagnostics") end,
-            { desc = "Workspace diagnostics" })
-        vim.keymap.set("n", "<leader>xd", function() trouble.toggle("document_diagnostics") end,
-            { desc = "Document diagnostics" })
-        vim.keymap.set("n", "<leader>xq", function() trouble.toggle("quickfix") end, { desc = "Quickfix" })
-        vim.keymap.set("n", "<leader>xl", function() trouble.toggle("loclist") end, { desc = "Loclist" })
-    end
 }
