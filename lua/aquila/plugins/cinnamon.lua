93d3e0e778d085cb330177133695b74acd3fa1f1
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-03-18 19:22:40 +0800

    chore: enable cinnnamon plug

--- lua/aquila/plugins/cinnamon.lua
+++ lua/aquila/plugins/cinnamon.lua
@@ -1,18 +1,18 @@
 return {
     'declancm/cinnamon.nvim',
-    event = 'BufRead',
-    enabled = false,
+    event = { "BufReadPost", "BufNewFile" },
     config = function()
         require('cinnamon').setup({
             default_keymaps = true,
             extra_keymaps = true,
             override_keymaps = true,
             extended_keymaps = true,
-            max_length = 500,
-
+            max_length = 50,
+            centered = false,
+            scroll_limit = -1
         })
         -- Horizontal screen scrolling:
-        vim.keymap.set('n', '<A-h>', "<Cmd>lua Scroll('z10h')<CR>")
-        vim.keymap.set('n', '<A-l>', "<Cmd>lua Scroll('z10l')<CR>")
+        vim.keymap.set('n', 'zh', "<Cmd>lua Scroll('z15h')<CR>")
+        vim.keymap.set('n', 'zl', "<Cmd>lua Scroll('z15l')<CR>")
     end
 }
