09faeba2da5e95af45dc045996e241105b0396f9
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-03-21 03:27:35 +0800

    feat: add cpy_buffers plugin

---
+++ lua/aquila/plugins/telescope/cpy_buffers.lua
@@ -0,0 +1,15 @@
+return {
+    'adia-dev/cpy_buffers.nvim',
+    dependencies = {
+        'nvim-telescope/telescope.nvim',
+    },
+    lazy = true,
+    keys = {
+        {
+            "<Leader>fc",
+            '<cmd>lua require("cpy_buffers.picker").open_file_picker<CR>',
+            desc = 'Copy from buffer to file'
+        },
+    },
+    opts = {},
+}
