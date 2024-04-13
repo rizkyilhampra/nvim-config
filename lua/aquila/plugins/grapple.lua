d336b556ea8b331c1ed9edf62e4bfe95a5663341
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-04-14 02:22:17 +0800

    refactor(grapple): remove brackets and lazy option

--- lua/aquila/plugins/grapple.lua
+++ lua/aquila/plugins/grapple.lua
@@ -1,7 +1,7 @@
 return {
     "cbochs/grapple.nvim",
     dependencies = {
-        { "nvim-tree/nvim-web-devicons", lazy = true }
+        "nvim-tree/nvim-web-devicons"
     },
     opts = {
         scope = "git", -- also try out "git_branch"
