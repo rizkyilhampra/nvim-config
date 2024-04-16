0805115f4e8f64d14794b7a26b4d07d089bd5991
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-04-16 15:22:53 +0800

    refactor: copilot plugin

--- lua/aquila/plugins/copilot.lua
+++ lua/aquila/plugins/copilot.lua
@@ -2,11 +2,12 @@ return {
     "zbirenbaum/copilot.lua",
     cmd = "Copilot",
     event = "InsertEnter",
-    config = function()
-        require("copilot").setup({
-            suggestion = {
-                auto_trigger = true,
-            }
-        })
-    end,
+    opts = {
+        suggestion = {
+            auto_trigger = true,
+        },
+        filetypes = {
+            markdown = true
+        }
+    }
 }
