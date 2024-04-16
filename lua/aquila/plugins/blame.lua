4b187fc1636a10b5804175639d60e27b6980fc3e
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-04-17 04:11:49 +0800

    feat: add blame plugin

---
+++ lua/aquila/plugins/blame.lua
@@ -0,0 +1,13 @@
+return {
+    "FabijanZulj/blame.nvim",
+    opts = {},
+    cmd = "BlameToggle",
+    keys = {
+        {
+            "<Leader>gb",
+            "<cmd>BlameToggle<CR>",
+            desc = "Toggle git blame of current file"
+        }
+
+    }
+}
