5051b44d8dd37f2cc0b80f216e02b07c4b58d4ef
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-07-19 16:19:29 +0800

    chore(notify): changed timeout, size and added markdown support

--- lua/aquila/plugins/notify.lua
+++ lua/aquila/plugins/notify.lua
@@ -1,8 +1,31 @@
 return {
     "rcarriga/nvim-notify",
+    event = "User BaseDefered",
     opts = {
-        max_width = math.floor(vim.o.columns * 0.4),
+        timeout = 500,
+        max_height = function() return math.floor(vim.o.lines * 0.75) end,
+        max_width = function() return math.floor(vim.o.columns * 0.75) end,
         render = "wrapped-compact",
-        timeout = 200
-    }
+        on_open = function(win)
+            -- enable markdown support on notifications
+            vim.api.nvim_win_set_config(win, { zindex = 175 })
+            if not vim.g.notifications_enabled then
+                vim.api.nvim_win_close(win, true)
+            end
+            if not package.loaded["nvim-treesitter"] then
+                pcall(require, "nvim-treesitter")
+            end
+            vim.wo[win].conceallevel = 3
+            local buf = vim.api.nvim_win_get_buf(win)
+            if not pcall(vim.treesitter.start, buf, "markdown") then
+                vim.bo[buf].syntax = "markdown"
+            end
+            vim.wo[win].spell = false
+        end,
+    },
+    config = function(_, opts)
+        local notify = require("notify")
+        notify.setup(opts)
+        vim.notify = notify
+    end,
 }
