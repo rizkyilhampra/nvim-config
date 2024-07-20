6b434233443ba55e0d386bbbf71aaefdd96b4e6e
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-07-20 12:22:28 +0800

    chore(grapple): some changed

--- lua/aquila/plugins/grapple.lua
+++ lua/aquila/plugins/grapple.lua
@@ -4,12 +4,12 @@ return {
         "nvim-tree/nvim-web-devicons"
     },
     opts = {
-        scope = "git", -- also try out "git_branch"
+        scope = "git",
     },
     cmd = "Grapple",
     keys = {
         {
-            "<Leader><GG>", "<cmd>Grapple toggle_tags<CR>", desc = "Grapple toggle tags",
+            "<Leader>GG", "<cmd>Grapple toggle_tags<CR>", desc = "Grapple toggle tags",
         },
         {
             "<Leader>GA", "<cmd>Grapple toggle<CR>", desc = "Grapple toggle tag"
