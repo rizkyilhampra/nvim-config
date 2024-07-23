541ea83c808ff51f095d316771d119d99f971086
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-07-23 13:59:17 +0800

    chore(image): fix magick not found and updated plug

--- lua/aquila/plugins/image.lua
+++ lua/aquila/plugins/image.lua
@@ -1,5 +1,10 @@
 return {
     "3rd/image.nvim",
     event = "User BaseDefered",
-    opts = {}
+    opts = {},
+    config = function(_, opts)
+        require("image").setup(opts)
+        package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua"
+        package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?.lua"
+    end,
 }
