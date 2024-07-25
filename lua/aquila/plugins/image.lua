260ef3ec49261c8be7a7841ae448420422d719d3
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-07-26 00:57:32 +0800

    chore(image): disabled

--- lua/aquila/plugins/image.lua
+++ lua/aquila/plugins/image.lua
@@ -1,7 +1,10 @@
 return {
     "3rd/image.nvim",
+    enabled = false,
     event = "User BaseDefered",
-    opts = {},
+    opts = {
+        tmux_show_only_in_active_window = true
+    },
     config = function(_, opts)
         require("image").setup(opts)
         package.path = package.path .. ";" .. vim.fn.expand("$HOME") .. "/.luarocks/share/lua/5.1/?/init.lua"
