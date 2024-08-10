f00fef473202156a8befe2c2e1a7f0361e4e43bb
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-08-11 07:12:36 +0800

    chore(treesitter): replace pattern with extension for http file

--- lua/aquila/plugins/nvim-treesitter.lua
+++ lua/aquila/plugins/nvim-treesitter.lua
@@ -65,13 +65,13 @@ return {
         vim.filetype.add({
             extension = {
                 neon = 'yaml',
+                ['http'] = 'http'
             },
             pattern = {
                 ['.*%.neon%.dist'] = 'yaml',
                 ['.*/waybar/config'] = 'jsonc',
                 ['.*/kitty/*.conf'] = 'bash',
                 ['.*/hypr/.*%.conf'] = 'hyprlang',
-                ['.*%.http%'] = 'http',
             },
         })
     end
