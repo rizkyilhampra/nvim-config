c030183cd83ccbd5b0e894fab873bd3021de4311
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-09-21 15:03:02 +0800

    feat(autopairs): adding space between {{}}

--- lua/aquila/plugins/nvim-autopairs.lua
+++ lua/aquila/plugins/nvim-autopairs.lua
@@ -28,5 +28,22 @@ return {
                     tex = false }
             )
         end
+
+        local brackets = {
+            { '{{', '}}' }
+        }
+        local Rule = require('nvim-autopairs.rule')
+        local cond = require('nvim-autopairs.conds')
+        npairs.add_rules {
+            Rule(' ', ' ')
+                :with_pair(function(pairs)
+                    local pair = pairs.line:sub(pairs.col - 2, pairs.col + 1)
+                    return vim.tbl_contains({
+                        brackets[1][1] .. brackets[1][2],
+                    }, pair)
+                end)
+                :with_move(cond.none())
+                :with_cr(cond.none())
+        }
     end
 }
