6c8b1656eb633d0e68ec1454dd79eb12f417c5d2
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-09-23 16:32:51 +0800

    feat: added truncateline plug

---
+++ lua/aquila/plugins/truncateline.lua
@@ -0,0 +1,11 @@
+return {
+    "rlychrisg/truncateline.nvim",
+    ft = "markdown",
+    opts = {
+        enabled_on_start = true,
+        truncate_str = "...",
+        line_start_length = 8,
+        temporary_toggle_dur = 2000,
+        hilight_group = "Comment",
+    },
+}
