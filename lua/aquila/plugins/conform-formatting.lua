c0c95a02e12cc8235b0ca597b693b6b2b97c2570
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-10-04 19:55:23 +0800

    chore(conform): added html to use prettierd

--- lua/aquila/plugins/conform-formatting.lua
+++ lua/aquila/plugins/conform-formatting.lua
@@ -24,6 +24,7 @@ return {
             json = { "prettierd" },
             typescript = { "prettierd" },
             typescriptreact = { "prettierd" },
+            html = { "prettierd" },
             blade = function(_)
                 -- NOTE: formatting blade files with prettierd it's use https://github.com/shufo/prettier-plugin-blade
                 -- so it's need has .prettierrc file in the project root and also configured with that plugin.
