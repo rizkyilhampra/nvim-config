5c35ca2a8119f2dd145c31ef260d541527a6d853
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-10-19 22:25:02 +0800

    chore(luminate): changed event

--- lua/aquila/plugins/luminate.lua
+++ lua/aquila/plugins/luminate.lua
@@ -1,6 +1,6 @@
--- NOTE: Highlight yanked, pasted, and undone/redone text in Neovim with a splash of color. 
+-- NOTE: Highlight yanked, pasted, and undone/redone text in Neovim with a splash of color.
 return {
     'mei28/luminate.nvim',
-    event = "User BaseDefered",
+    event = "User BaseFile",
     opts = {}
 }
