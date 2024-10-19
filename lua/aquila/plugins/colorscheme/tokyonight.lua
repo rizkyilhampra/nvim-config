76fb0fe868b9c40bd715f6a96b71a8d6c0a7e464
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-10-19 18:49:45 +0800

    chore(tokyonight): clean up

--- lua/aquila/plugins/colorscheme/tokyonight.lua
+++ lua/aquila/plugins/colorscheme/tokyonight.lua
@@ -1,25 +1,24 @@
 return {
-    "folke/tokyonight.nvim",
-    event = "User LoadColorSchemes",
-    enabled = true,
-    opts = {
-        style = "moon",
-        transparent = true,
-        styles = {
-            -- sidebars = "transparent",
-            floats = "transparent"
-        },
-        terminal_colors = false,
-        on_highlights = function(hl, C)
-            hl.NormalMoody = { fg = C.blue }
-            hl.InsertMoody = { fg = C.green }
-            hl.VisualMoody = { fg = C.magenta }
-            hl.SelectMoody = { fg = C.magenta }
-            hl.OperatorMoody = { fg = C.yellow }
-            hl.CommandMoody = { fg = C.yellow }
-            hl.ReplaceMoody = { fg = C.red }
-            hl.TerminalMoody = { fg = C.green1 }
-            hl.TerminalNormalMoody = { fg = C.green1 }
-        end
-    },
+	"folke/tokyonight.nvim",
+	event = "User LoadColorSchemes",
+	enabled = true,
+	opts = {
+		style = "moon",
+		transparent = true,
+		styles = {
+			floats = "transparent",
+		},
+		terminal_colors = false,
+		on_highlights = function(hl, C)
+			hl.NormalMoody = { fg = C.blue }
+			hl.InsertMoody = { fg = C.green }
+			hl.VisualMoody = { fg = C.magenta }
+			hl.SelectMoody = { fg = C.magenta }
+			hl.OperatorMoody = { fg = C.yellow }
+			hl.CommandMoody = { fg = C.yellow }
+			hl.ReplaceMoody = { fg = C.red }
+			hl.TerminalMoody = { fg = C.green1 }
+			hl.TerminalNormalMoody = { fg = C.green1 }
+		end,
+	},
 }
