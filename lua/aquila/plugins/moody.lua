bb002638705bdc2a55024452a79e5abb407fc893
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-10-19 18:50:12 +0800

    chore(moody): clean up

--- lua/aquila/plugins/moody.lua
+++ lua/aquila/plugins/moody.lua
@@ -1,10 +1,13 @@
 return {
-    "svampkorg/moody.nvim",
-    event = "User BaseFile",
-    opts = {
-        disabled_filetypes = { "TelescopePrompt", "neo-tree", "neo-tree-popup" },
-        blends = {
-            visual = 0.15
-        }
-    },
+	"svampkorg/moody.nvim",
+	event = "User BaseDefered",
+	opts = {
+		disabled_filetypes = {
+			"neo-tree",
+			"neo-tree-popup",
+		},
+		blends = {
+			visual = 0.15,
+		},
+	},
 }
