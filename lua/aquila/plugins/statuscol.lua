eced7cf38730b2b3bf7d086a399360c9c44ee452
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-10-19 22:34:08 +0800

    chore(statuscol): clean up and changed event

--- lua/aquila/plugins/statuscol.lua
+++ lua/aquila/plugins/statuscol.lua
@@ -1,17 +1,17 @@
 return {
-    "luukvbaal/statuscol.nvim",
-    event = "VimEnter",
-    opts = function()
-        local builtin = require("statuscol.builtin")
-        return {
-            ft_ignore = { "alpha" },
-            segments = {
-                relculright = true,
-                { sign = { namespace = { "diagnostic*" }, auto = true }, click = "v:lua.ScSa" },
-                { sign = { namespace = { "gitsigns" }, auto = true, },   click = "v:lua.ScSa", },
-                { text = { builtin.lnumfunc, " " },                      click = "v:lua.ScLa", },
-                { text = { builtin.foldfunc, "  " },                     click = "v:lua.ScFa", },
-            },
-        }
-    end
+	"luukvbaal/statuscol.nvim",
+	event = "BufWinEnter",
+	opts = function()
+		local builtin = require("statuscol.builtin")
+		return {
+			ft_ignore = { "alpha" },
+			segments = {
+				relculright = true,
+				{ sign = { namespace = { "diagnostic*" }, auto = true }, click = "v:lua.ScSa" },
+				{ sign = { namespace = { "gitsigns" }, auto = true }, click = "v:lua.ScSa" },
+				{ text = { builtin.lnumfunc, " " }, click = "v:lua.ScLa" },
+				{ text = { builtin.foldfunc, "  " }, click = "v:lua.ScFa" },
+			},
+		}
+	end,
 }
