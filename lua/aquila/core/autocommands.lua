d24f495811cada418798db6bdafcca474988f2a7
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-10-21 01:00:25 +0800

    chore(autocommands): added some

--- lua/aquila/core/autocommands.lua
+++ lua/aquila/core/autocommands.lua
@@ -225,4 +225,20 @@ M.create({ "VimEnter", "FileType", "BufEnter", "WinEnter" }, {
 	end,
 })
 
+M.create("BufWinEnter", {
+	desc = "Make q close help, man, quickfix, dap floats",
+	callback = function(args)
+		local buftype = vim.api.nvim_get_option_value("buftype", { buf = args.buf })
+		if vim.tbl_contains({ "help", "nofile", "quickfix" }, buftype) then
+			vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = args.buf, silent = true, nowait = true })
+		end
+	end,
+})
+M.create("CmdwinEnter", {
+	desc = "Make q close command history (q: and q?)",
+	callback = function(args)
+		vim.keymap.set("n", "q", "<cmd>close<cr>", { buffer = args.buf, silent = true, nowait = true })
+	end,
+})
+
 return M
