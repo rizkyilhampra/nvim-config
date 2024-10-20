be910e5e3c0d8339edb66dec6dcb52677914dab4
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-10-21 01:07:10 +0800

    chore(mini-animate): changed event and clean up

--- lua/aquila/plugins/mini-animate.lua
+++ lua/aquila/plugins/mini-animate.lua
@@ -1,40 +1,37 @@
 return {
-    'echasnovski/mini.animate',
-    event = "User BaseDefered",
-    version = false,
-    opts = function()
-        -- don't use animate when scrolling with the mouse
-        local mouse_scrolled = false
-        for _, scroll in ipairs { "Up", "Down" } do
-            local key = "<ScrollWheel" .. scroll .. ">"
-            vim.keymap.set({ "", "i" }, key, function()
-                mouse_scrolled = true
-                return key
-            end, { expr = true })
-        end
+	"echasnovski/mini.animate",
+	event = "User BaseFile",
+	version = false,
+	opts = function()
+		local mouse_scrolled = false
+		for _, scroll in ipairs({ "Up", "Down" }) do
+			local key = "<ScrollWheel" .. scroll .. ">"
+			vim.keymap.set({ "", "i" }, key, function()
+				mouse_scrolled = true
+				return key
+			end, { expr = true })
+		end
 
-        local animate = require("mini.animate")
-        return {
-            open = { enable = false }, -- true causes issues on nvim-spectre
-            resize = {
-                timing = animate.gen_timing.linear { duration = 33, unit = "total" },
-            },
-            close = { enable = false },
-            scroll = {
-                timing = animate.gen_timing.linear { duration = 40, unit = "total" },
-                subscroll = animate.gen_subscroll.equal {
-                    predicate = function(total_scroll)
-                        if mouse_scrolled then
-                            mouse_scrolled = false
-                            return false
-                        end
-                        return total_scroll > 1
-                    end,
-                },
-            },
-            cursor = {
-                enable = false, -- We don't want cursor ghosting
-            },
-        }
-    end,
+		local animate = require("mini.animate")
+		return {
+			open = { enable = false },
+			resize = {
+				timing = animate.gen_timing.linear({ duration = 33, unit = "total" }),
+			},
+			close = { enable = false },
+			scroll = {
+				timing = animate.gen_timing.linear({ duration = 50, unit = "total" }),
+				subscroll = animate.gen_subscroll.equal({
+					predicate = function(total_scroll)
+						if mouse_scrolled then
+							mouse_scrolled = false
+							return false
+						end
+						return total_scroll > 1
+					end,
+				}),
+			},
+			cursor = { enable = false },
+		}
+	end,
 }
