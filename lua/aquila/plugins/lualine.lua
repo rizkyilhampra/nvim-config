a90b4e9012f50c79b030a83fe899c6c7344daa6a
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-10-26 04:38:11 +0800

    chore(lualine): removed separators and formatted using stylua

--- lua/aquila/plugins/lualine.lua
+++ lua/aquila/plugins/lualine.lua
@@ -1,78 +1,77 @@
 local mode_map = {
-    n = "(ᴗ_ ᴗ。)",
-    nt = "(ᴗ_ ᴗ。)",
-    i = "(•̀ - •́ )",
-    R = "( •̯́ ₃ •̯̀)",
-    v = "(⊙ _ ⊙ )",
-    V = "(⊙ _ ⊙ )",
-    no = "Σ(°△°ꪱꪱꪱ)",
-    ["\22"] = "(⊙ _ ⊙ )",
-    t = "(⌐■_■)",
-    ['!'] = "Σ(°△°ꪱꪱꪱ)",
-    c = "Σ(°△°ꪱꪱꪱ)",
-    s = "SUB"
+	n = "(ᴗ_ ᴗ。)",
+	nt = "(ᴗ_ ᴗ。)",
+	i = "(•̀ - •́ )",
+	R = "( •̯́ ₃ •̯̀)",
+	v = "(⊙ _ ⊙ )",
+	V = "(⊙ _ ⊙ )",
+	no = "Σ(°△°ꪱꪱꪱ)",
+	["\22"] = "(⊙ _ ⊙ )",
+	t = "(⌐■_■)",
+	["!"] = "Σ(°△°ꪱꪱꪱ)",
+	c = "Σ(°△°ꪱꪱꪱ)",
+	s = "SUB",
 }
 
 return {
-    'nvim-lualine/lualine.nvim',
-    event = "User BaseDefered",
-    opts = function()
-        return {
-            options = {
-                disabled_filetypes = {
-                    statusline = {
-                        "alpha",
-                        "neo-tree",
-                        "neo-tree-popup",
-                        "TelescopePrompt",
-                        "toggleterm",
-                        "minimap"
-                    },
-                },
-                component_separators = '|',
-                section_separators = { left = '', right = '' },
-                -- section_separators = { left = '', right = '' },
-            },
-            sections = {
-                lualine_a = {
-                    {
-                        'mode',
-                        icons_enabled = true,
-                        separator = {
-                            right = ""
-                        },
-                        fmt = function()
-                            return mode_map[vim.api.nvim_get_mode().mode] or vim.api.nvim_get_mode().mode
-                        end
-                    },
-                },
-                lualine_b = { { 'branch', icon = ' ' } },
-                lualine_c = {
-                    {
-                        'diff',
-                        symbols = require('aquila.core.global').icons.git,
-                    },
-                    'diagnostics'
-                },
-                lualine_x = {
-                    'filetype',
-                    {
-                        'o:encoding',
-                        fmt = string.upper,
-                    },
-                    {
-                        'fileformat',
-                        symbols = {
-                            unix = 'LF',
-                            dos = 'CRLF',
-                            mac = 'CR',
-                        },
-                        fmt = string.upper,
-                    },
-                },
-                lualine_y = { 'progress' },
-                lualine_z = { 'location' },
-            },
-        }
-    end
+	"nvim-lualine/lualine.nvim",
+	event = "User BaseDefered",
+	opts = function()
+		return {
+			options = {
+				disabled_filetypes = {
+					statusline = {
+						"alpha",
+						"neo-tree",
+						"neo-tree-popup",
+						"TelescopePrompt",
+						"toggleterm",
+						"minimap",
+					},
+				},
+				component_separators = "",
+				section_separators = { left = "", right = "" },
+			},
+			sections = {
+				lualine_a = {
+					{
+						"mode",
+						icons_enabled = true,
+						separator = {
+							right = "",
+						},
+						fmt = function()
+							return mode_map[vim.api.nvim_get_mode().mode] or vim.api.nvim_get_mode().mode
+						end,
+					},
+				},
+				lualine_b = { { "branch", icon = " " } },
+				lualine_c = {
+					{
+						"diff",
+						symbols = require("aquila.core.global").icons.git,
+					},
+					"diagnostics",
+				},
+				lualine_x = {
+					"filetype",
+					{
+						"o:encoding",
+						fmt = string.upper,
+					},
+					{
+						"fileformat",
+						symbols = {
+							unix = "LF",
+							dos = "CRLF",
+							mac = "CR",
+						},
+						fmt = string.upper,
+					},
+				},
+				lualine_y = { "progress" },
+				lualine_z = { "location" },
+			},
+		}
+	end,
 }
