59d74df3ab909b659139e0d89b891b09d2b951e8
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-10-26 05:18:51 +0800

    feat: better lua workflow with lazydev and related dependencies

---
+++ lua/aquila/plugins/lazydev.lua
@@ -0,0 +1,21 @@
+return {
+	"folke/lazydev.nvim",
+	ft = "lua",
+	dependencies = {
+		"Bilal2453/luvit-meta",
+		{ "LuaCATS/luassert", name = "luassert-types" },
+		{ "LuaCATS/busted", name = "busted-types" },
+	},
+	opts = {
+		library = {
+			{ path = "lazy.nvim", mods = { "lazy" } },
+			{ path = "garbage-day.nvim", mods = { "garbage-day" } },
+			{ path = "lazydev.nvim", mods = { "" } },
+			{ path = "luvit-meta/library", words = { "vim%.uv" } },
+			{ path = "luassert-types/library", words = { "assert" } },
+			{ path = "busted-types/library", words = { "describe" } },
+			{ path = "nvimtools/none-ls.nvim", words = { "null-ls" } },
+			"malas-ngodonf.nvim",
+		},
+	},
+}
