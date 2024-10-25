0ecf8a9f0058a9aab24dbd1b5f97f6823c876e89
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-10-26 04:38:46 +0800

    chore(none-ls): changed event and formatted using stylua

--- lua/aquila/plugins/lsp/none-ls.lua
+++ lua/aquila/plugins/lsp/none-ls.lua
@@ -1,34 +1,31 @@
 return {
-    'nvimtools/none-ls.nvim',
-    event = {
-        "BufReadPost",
-        "BufNewFile"
-    },
-    dependencies = {
-        "williamboman/mason.nvim",
-        "nvimtools/none-ls-extras.nvim",
-    },
-    config = function()
-        local null_ls = require('null-ls')
+	"nvimtools/none-ls.nvim",
+	event = "User BaseFile",
+	dependencies = {
+		"williamboman/mason.nvim",
+		"nvimtools/none-ls-extras.nvim",
+	},
+	config = function()
+		local null_ls = require("null-ls")
 
-        local sources = {
-            require("none-ls.diagnostics.eslint_d").with({
-                condition = function(utils)
-                    -- Check if the root directory has a ".eslintrc.{js,yml,json}" file
-                    local filenames = { ".eslintrc.js", ".eslintrc.yml", ".eslintrc.json" }
-                    for _, filename in ipairs(filenames) do
-                        if utils.root_has_file(filename) then
-                            return true
-                        end
-                    end
-                    return false
-                end,
-            }),
-            null_ls.builtins.diagnostics.selene,
-        }
+		local sources = {
+			require("none-ls.diagnostics.eslint_d").with({
+				condition = function(utils)
+					-- Check if the root directory has a ".eslintrc.{js,yml,json}" file
+					local filenames = { ".eslintrc.js", ".eslintrc.yml", ".eslintrc.json" }
+					for _, filename in ipairs(filenames) do
+						if utils.root_has_file(filename) then
+							return true
+						end
+					end
+					return false
+				end,
+			}),
+			null_ls.builtins.diagnostics.selene,
+		}
 
-        null_ls.setup({
-            sources = sources,
-        })
-    end
+		null_ls.setup({
+			sources = sources,
+		})
+	end,
 }
