37f7f830d4b0c550a6e12d2b4b6ccf5803361704
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-11-03 15:42:18 +0800

    chore(snipe): follow breaking changes

--- lua/aquila/plugins/snipe.lua
+++ lua/aquila/plugins/snipe.lua
@@ -1,9 +1,13 @@
 return {
-    "leath-dub/snipe.nvim",
-    event = "User BaseFile",
-    config = function()
-        local snipe = require("snipe")
-        snipe.setup()
-        vim.keymap.set("n", "gb", snipe.create_buffer_menu_toggler())
-    end
+	"leath-dub/snipe.nvim",
+	keys = {
+		{
+			"gb",
+			function()
+				require("snipe").open_buffer_menu()
+			end,
+			desc = "Open Snipe buffer menu",
+		},
+	},
+	opts = {},
 }
