4a03d3e79c738029a44d7fe3801948390361b3d6
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-11-07 22:24:03 +0800

    chore(tailwind-tools): added condition for loaded

--- lua/aquila/plugins/tailwind-tools.lua
+++ lua/aquila/plugins/tailwind-tools.lua
@@ -1,14 +1,12 @@
 return {
-    "luckasRanarison/tailwind-tools.nvim",
-    name = "tailwind-tools",
-    build = ":UpdateRemotePlugins",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "nvim-telescope/telescope.nvim",
-    },
-    opts = {
-        conceal = {
-            enabled = false,
-        },
-    }
+	"luckasRanarison/tailwind-tools.nvim",
+	name = "tailwind-tools",
+	build = ":UpdateRemotePlugins",
+	event = "User BaseFile",
+	cond = vim.fn.filereadable("tailwind.config.js") == 1,
+	opts = {
+		conceal = {
+			enabled = false,
+		},
+	},
 }
