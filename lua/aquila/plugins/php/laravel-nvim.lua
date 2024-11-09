b6d5f33d16bf87cbc69d92e7ea5f774114b8746d
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-11-10 04:26:11 +0800

    chore(laravel-nvim): removed telescope from dependency

--- lua/aquila/plugins/php/laravel-nvim.lua
+++ lua/aquila/plugins/php/laravel-nvim.lua
@@ -2,7 +2,6 @@ return {
     "adalessa/laravel.nvim",
     dependencies = {
         "tpope/vim-dotenv",
-        "nvim-telescope/telescope.nvim",
         "MunifTanjim/nui.nvim",
         "kevinhwang91/promise-async",
     },
