3935afa03220f4e2bccb40e33c4adda0268fb4f9
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-11-13 04:25:33 +0800

    chore(codesnap): changed into cmd instead of BufRead event

--- lua/aquila/plugins/codesnap.lua
+++ lua/aquila/plugins/codesnap.lua
@@ -1,12 +1,13 @@
 return {
-    "mistricky/codesnap.nvim",
-    build = "make",
-    event = "BufReadPost",
-    config = function()
-        require("codesnap").setup({
-            watermark = "",
-            editor_font_familiy = "Iosevka Expanded",
-            bg_padding = 0
-        })
-    end
+	"mistricky/codesnap.nvim",
+	build = "make",
+	cmd = "CodeSnap",
+	config = function()
+		require("codesnap").setup({
+			watermark = "",
+			editor_font_familiy = "Iosevka Expanded",
+			bg_padding = 0,
+		})
+	end,
 }
+
