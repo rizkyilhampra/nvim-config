e8dfba66c8f5843609eca42678cba7d22596d737
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-11-19 01:07:07 +0800

    chore(bufremove): remap for <C-w>q

--- lua/aquila/plugins/mini-buffremove.lua
+++ lua/aquila/plugins/mini-buffremove.lua
@@ -1,4 +1,16 @@
 return {
-    "echasnovski/mini.bufremove",
-    event = "User BaseFile"
+	"echasnovski/mini.bufremove",
+	event = "User BaseFile",
+	opts = {
+		silent = true,
+	},
+	keys = {
+		{
+			"<C-w>q",
+			":lua MiniBufremove.delete()<CR>",
+			noremap = true,
+			silent = true,
+			desc = "Close buffer",
+		},
+	},
 }
