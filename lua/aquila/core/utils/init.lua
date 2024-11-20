1793aaf6afce906062ec3789c2f959b0d567c281
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-11-21 03:49:29 +0800

    feat(autocommands): add async keyword automatically for js/ts

--- lua/aquila/core/utils/init.lua
+++ lua/aquila/core/utils/init.lua
@@ -326,4 +326,21 @@ function M.get_indent(lines)
 	return indent
 end
 
+---@param types string[] Will return the first node that matches one of these types
+---@param node TSNode|nil
+---@return TSNode|nil
+function M.find_node_ancestor(types, node)
+	if not node then
+		return nil
+	end
+
+	if vim.tbl_contains(types, node:type()) then
+		return node
+	end
+
+	local parent = node:parent()
+
+	return M.find_node_ancestor(types, parent)
+end
+
 return M
