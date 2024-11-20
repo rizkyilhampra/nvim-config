bc1cf63f946e9a164ff02e7df0e6dc466df7b2b1
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-11-20 08:29:47 +0800

    chore: added ensure_installed table to mason-lspconfig

--- lua/aquila/plugins/lsp/mason-lspconfig.lua
+++ lua/aquila/plugins/lsp/mason-lspconfig.lua
@@ -1,3 +1,19 @@
+local servers = {
+	"intelephense",
+	"lua_ls",
+	"cssls",
+	"html",
+	"vtsls",
+	"emmet_ls",
+	"bashls",
+	"marksman",
+	"jsonls",
+	"sqlls",
+	"yamlls",
+	"tailwindcss",
+	"docker_compose_language_service",
+	"dockerls",
+}
 local utils = require("aquila.core.utils")
 local utils_lsp = require("aquila.core.utils.lsp")
 
@@ -7,7 +23,6 @@ return {
 		"neovim/nvim-lspconfig",
 	},
 	event = "User BaseFile",
-
 	opts = function(_, opts)
 		if not opts.handlers then
 			opts.handlers = {}
@@ -15,6 +30,7 @@ return {
 		opts.handlers[1] = function(server)
 			utils_lsp.setup(server)
 		end
+		opts.ensure_installed = servers
 	end,
 	config = function(_, opts)
 		require("mason-lspconfig").setup(opts)
