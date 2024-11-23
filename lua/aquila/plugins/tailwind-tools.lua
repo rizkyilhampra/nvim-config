82d31a09531196da9d232d2a55361d630ce5f22d
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-11-23 16:09:11 +0800

    chore(tailwind-tools): removed event and set server.override to false

--- lua/aquila/plugins/tailwind-tools.lua
+++ lua/aquila/plugins/tailwind-tools.lua
@@ -2,9 +2,11 @@ return {
 	"luckasRanarison/tailwind-tools.nvim",
 	name = "tailwind-tools",
 	build = ":UpdateRemotePlugins",
-	event = "User BaseFile",
 	cond = vim.fn.filereadable("tailwind.config.js") == 1,
 	opts = {
+		server = {
+			override = false,
+		},
 		conceal = {
 			enabled = false,
 		},
