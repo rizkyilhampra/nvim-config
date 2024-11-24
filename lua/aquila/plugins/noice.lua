4835d9b0ede35d752fd2e9d8e927d8fe24400e59
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-11-24 17:49:38 +0800

    chore(noice): enabled stylize_markdown and removed notice eagle.nvim issue

--- lua/aquila/plugins/noice.lua
+++ lua/aquila/plugins/noice.lua
@@ -37,7 +37,7 @@ return {
             -- override markdown rendering so that **cmp** and other plugins use **Treesitter**
             override = {
                 ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
-                ["vim.lsp.util.stylize_markdown"] = false, -- NOTE: This is a workaround for an issue with eagle.nvim
+                ["vim.lsp.util.stylize_markdown"] = true,
                 ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
             },
             progress = {
