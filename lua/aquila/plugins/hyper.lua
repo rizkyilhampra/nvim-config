2328ca1c35e5810c89d8728a91d98f4465692253
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-11-25 21:38:46 +0800

    chore: deleted some of plugin caused can't follow breaking changes

--- lua/aquila/plugins/hyper.lua
+++
@@ -1,8 +0,0 @@
-return {
-    'wet-sandwich/hyper.nvim',
-    dependencies = { 'nvim-lua/plenary.nvim' },
-    cmd = "Hyper",
-    keys = {
-        { '<leader>H', "<cmd>Hyper<CR>", desc = 'Show Hyper' },
-    }
-}
