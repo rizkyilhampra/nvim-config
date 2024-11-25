196e548ce22ef0078d11468dd54300bb7ab973d0
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-11-25 19:03:42 +0800

    chore(phpactor): disable phpstan and auto config

--- lua/aquila/core/utils/lsp/servers/phpactor.lua
+++ lua/aquila/core/utils/lsp/servers/phpactor.lua
@@ -4,8 +4,8 @@ local lspconfig = require("lspconfig")
 
 M.init_options = {
 	["language_server_worse_reflection.inlay_hints.enable"] = true,
-	["language_server_phpstan.enabled"] = true,
-	["phpunit.enabled"] = true,
+	["language_server_phpstan.enabled"] = false,
+	["language_server_configuration.auto_config"] = false,
 }
 M.root_dir = function(fname)
 	return lspconfig.util.root_pattern("composer.json", ".git")(fname) or lspconfig.util.path.dirname(fname)
