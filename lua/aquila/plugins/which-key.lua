e39755abe37e4928ff1c38d9ca411b55af7aecc1
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-11-26 15:25:06 +0800

    chore(which-key): refactored and formatted by stylua

--- lua/aquila/plugins/which-key.lua
+++ lua/aquila/plugins/which-key.lua
@@ -1,23 +1,25 @@
 return {
-    "folke/which-key.nvim",
-    event = "User BaseDefered",
-    opts = {
-        disable = { ft = { "TelescopePrompt", "noice", "toggleterm" } },
-        icons = {
-            mappings = false
-        }
-    },
-    config = function(_, opts)
-        require('which-key').setup(opts)
+	"folke/which-key.nvim",
+	event = "User BaseDefered",
+	opts = {
+		disable = { ft = { "TelescopePrompt", "noice", "toggleterm" } },
+		icons = {
+			mappings = false,
+		},
+	},
+	config = function(_, opts)
+		local which_key = require("which-key")
 
-        require('which-key').add({
-            { "<Leader>f",  group = "find/file" },
-            { "<Leader>fl", group = "lsp" },
-            { "<Leader>t",  group = "terminal" },
-            { "<Leader>x",  group = "diagnostic" },
-            { "<Leader>c",  group = "code" },
-            { "<Leader>g",  group = "git" },
-            { "<Leader>G",  group = "file tags" },
-        })
-    end
+		which_key.setup(opts)
+		which_key.add({
+			{ "<Leader>f", group = "find/file" },
+			{ "<Leader>fl", group = "lsp" },
+			{ "<Leader>t", group = "terminal" },
+			{ "<Leader>x", group = "diagnostic" },
+			{ "<Leader>c", group = "code" },
+			{ "<Leader>g", group = "git" },
+			{ "<Leader>G", group = "file tags" },
+		})
+	end,
 }
+
