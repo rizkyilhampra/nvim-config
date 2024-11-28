c2533e3bed9bba2462efaf995e1f7a39afbb8c03
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-11-29 00:54:25 +0800

    refactor: updated event in flash.nvim and rainbow-delimeter.nvim

--- lua/aquila/plugins/flash.lua
+++ lua/aquila/plugins/flash.lua
@@ -1,14 +1,15 @@
 return {
-    "folke/flash.nvim",
-    event = { "BufReadPre", "BufNewFile" },
-    opts = {
-        modes = {
-            search = {
-                enabled = false
-            },
-            char = {
-                keys = { "f", "F", "t", "T" },
-            }
-        }
-    },
+	"folke/flash.nvim",
+	event = "User BaseFile",
+	opts = {
+		modes = {
+			search = {
+				enabled = false,
+			},
+			char = {
+				keys = { "f", "F", "t", "T" },
+			},
+		},
+	},
 }
+
