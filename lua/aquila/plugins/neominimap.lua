b5c6abf4d6f400e26ddd62f2f0a0b636a37cebf6
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-12-05 00:04:45 +0800

    chore(neominimap): moved conf to global option and make loaded by certain event

--- lua/aquila/plugins/neominimap.lua
+++ lua/aquila/plugins/neominimap.lua
@@ -1,31 +1,4 @@
 return {
-    "Isrothy/neominimap.nvim",
-    lazy = false,
-    init = function()
-        vim.g.neominimap = {
-            diagnostic = {
-                enabled = false
-            },
-            auto_enable = true,
-            buf_filter = function(bufnr)
-                local line_count = vim.api.nvim_buf_line_count(bufnr)
-                return line_count < 4096
-            end,
-            float = {
-                z_index = 21,
-                margin = {
-                    top = 1,
-                },
-                window_border = "",
-                minimap_width = 15,
-            },
-            search = {
-                enabled = true
-
-            },
-            click = {
-                enabled = true,
-            }
-        }
-    end
+	"Isrothy/neominimap.nvim",
+	event = { "BufNewFile", "BufReadPre" },
 }
