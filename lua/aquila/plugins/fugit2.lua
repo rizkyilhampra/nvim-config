3cbe8bd7f964f2cfe79406fed3006b17ad9ee2cf
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-12-05 01:18:52 +0800

    chore(fugi2): some changes conf and dependencies

--- lua/aquila/plugins/fugit2.lua
+++ lua/aquila/plugins/fugit2.lua
@@ -1,52 +1,31 @@
 return {
-    'SuperBo/fugit2.nvim',
-    opts = {
-        width = 70,
-        height = "80%",
-        external_diffview = false,
-    },
-    dependencies = {
-        'MunifTanjim/nui.nvim',
-        'nvim-tree/nvim-web-devicons',
-        'nvim-lua/plenary.nvim',
-        {
-            'chrisgrieser/nvim-tinygit',
-            dependencies = {
-                'stevearc/dressing.nvim',
-                "nvim-telescope/telescope.nvim",
-                "rcarriga/nvim-notify",
-            },
-            keys = {
-                {
-                    "<Leader>gi",
-                    "<cmd>lua require('tinygit').interactiveStaging()<CR>",
-                    desc = "Interactive Staging"
-                },
-                {
-                    "<Leader>gc",
-                    "<cmd>lua require('tinygit').smartCommit({pushIfClean = true})<CR>",
-                    desc = "Smart Commit"
-                }
-            }
-        },
-        {
-            'sindrets/diffview.nvim',
-            dependencies = { 'nvim-tree/nvim-web-devicons' },
-            cmd = {
-                'DiffviewFileHistory',
-                'DiffviewOpen',
-                'DiffviewToggleFiles',
-                'DiffviewFocusFiles',
-                'DiffviewRefresh'
-            }
-        }
-    },
-    cmd = { 'Fugit2', 'Fugit2Blame', 'Fugit2Diff', 'Fugit2Graph' },
-    keys = {
-        {
-            "<Leader>gff",
-            "<cmd>Fugit2<CR>",
-            desc = "Fugit2"
-        }
-    }
+	"SuperBo/fugit2.nvim",
+	opts = {
+		width = 70,
+		height = "80%",
+	},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"nvim-lua/plenary.nvim",
+		"chrisgrieser/nvim-tinygit",
+		{
+			"sindrets/diffview.nvim",
+			cmd = {
+				"DiffviewFileHistory",
+				"DiffviewOpen",
+				"DiffviewToggleFiles",
+				"DiffviewFocusFiles",
+				"DiffviewRefresh",
+			},
+		},
+	},
+	cmd = { "Fugit2", "Fugit2Blame", "Fugit2Diff", "Fugit2Graph" },
+	keys = {
+		{
+			"<Leader>gff",
+			"<cmd>Fugit2<CR>",
+			desc = "Fugit2",
+		},
+	},
 }
