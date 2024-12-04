64af142ad3fcdc56c539b51114f9306cd35b2942
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-12-05 01:00:32 +0800

    feat: added doing plug

---
+++ lua/aquila/plugins/dooing.lua
@@ -0,0 +1,14 @@
+return {
+	"atiladefreitas/dooing",
+	opts = {
+		window = {
+			height = 10,
+		},
+		quick_keys = false,
+		keymaps = {
+			toggle_window = false,
+			remove_duplicates = false,
+		},
+	},
+	cmd = "Dooing",
+}
