a1591a7be7c5a69222373267d6246f542551f641
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-12-04 23:44:36 +0800

    chore: deleted nvim-ts-autotag caused it also dependence on nvim-autopairs

--- lua/aquila/plugins/nvim-ts-autotag.lua
+++
@@ -1,9 +0,0 @@
-return {
-    "windwp/nvim-ts-autotag",
-    dependencies = {
-        "nvim-treesitter/nvim-treesitter",
-        "windwp/nvim-autopairs"
-    },
-    event = "InsertEnter",
-    opts = {}
-}
