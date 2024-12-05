3009e654ae92b9b57eed7fcd434dbf58ff615074
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-12-06 03:12:49 +0800

    chore(goto-preview): refactored and clean up

--- lua/aquila/plugins/goto-preview.lua
+++ lua/aquila/plugins/goto-preview.lua
@@ -1,19 +1,16 @@
 return {
-    'rmagatti/goto-preview',
-    enabled = true,
-    event = 'User BaseFile',
-    config = function()
-        require('goto-preview').setup({
-            width = 80,
-            post_open_hook = function()
-                vim.api.nvim_buf_set_keymap(0, 'n', 'q', '<cmd>lua require("goto-preview").close_all_win()<CR>',
-                    { noremap = true, silent = true })
-                -- set wrap!
-                vim.api.nvim_win_set_option(0, 'wrap', true)
-            end
-        })
-
-        vim.keymap.set("n", "gp", "<cmd>lua require('goto-preview').goto_preview_definition()<CR>", { noremap = true })
-        vim.keymap.set("n", "<Esc>", "<cmd>lua require('goto-preview').close_all_win()<CR>", { noremap = true })
-    end
+	"rmagatti/goto-preview",
+	opts = {
+		width = 80,
+		post_open_hook = function(buf, _)
+			vim.api.nvim_buf_set_keymap(
+				buf,
+				"n",
+				"q",
+				'<cmd>lua require("goto-preview").close_all_win()<CR>',
+				{ noremap = true, silent = true }
+			)
+		end,
+		same_file_float_preview = false,
+	},
 }
