f589b0fa67503cad36ff0e9dbf22f9a747cce400
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-12-06 03:31:24 +0800

    chore(neominimap): moved again into spesific plugin conf instead global conf

--- lua/aquila/core/options.lua
+++ lua/aquila/core/options.lua
@@ -82,28 +82,3 @@ vim.g.url_effect_enabled = true -- Highlight URLs with an underline effect.
 
 vim.g.php_lsp = "phpactor" -- intelephense
 vim.g.phpstan_enabled = false
-
-vim.g.neominimap = {
-	diagnostic = {
-		enabled = false,
-	},
-	auto_enable = true,
-	buf_filter = function(bufnr)
-		local line_count = vim.api.nvim_buf_line_count(bufnr)
-		return line_count < 4096
-	end,
-	float = {
-		z_index = 21,
-		margin = {
-			top = 1,
-		},
-		window_border = "",
-		minimap_width = 15,
-	},
-	search = {
-		enabled = true,
-	},
-	click = {
-		enabled = true,
-	},
-}
--- lua/aquila/plugins/neominimap.lua
+++ lua/aquila/plugins/neominimap.lua
@@ -1,4 +1,34 @@
 return {
 	"Isrothy/neominimap.nvim",
-	event = { "BufNewFile", "BufReadPre" },
+	lazy = false,
+	init = function()
+		vim.g.neominimap = {
+			diagnostic = {
+				enabled = true,
+			},
+			auto_enable = true,
+			buf_filter = function(bufnr)
+				local line_count = vim.api.nvim_buf_line_count(bufnr)
+				return line_count < 4096
+			end,
+			float = {
+				z_index = 21,
+				margin = {
+					top = 1,
+				},
+				window_border = "",
+				minimap_width = 15,
+			},
+			search = {
+				enabled = true,
+			},
+			click = {
+				enabled = true,
+			},
+			delay = 1000,
+			git = {
+				enabled = false,
+			},
+		}
+	end,
 }
