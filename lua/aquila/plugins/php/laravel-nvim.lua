e0dc863776eba304fa09d3d84423edf784eba345
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-12-07 20:00:32 +0800

    chore(laravel-nvim): only loaded by command and disabled virt_text

--- lua/aquila/plugins/php/laravel-nvim.lua
+++ lua/aquila/plugins/php/laravel-nvim.lua
@@ -1,17 +1,21 @@
 return {
-    "adalessa/laravel.nvim",
-    dependencies = {
-        "tpope/vim-dotenv",
-        "MunifTanjim/nui.nvim",
-        "kevinhwang91/promise-async",
-    },
-    cmd = { "Laravel" },
-    keys = {
-        { "<leader>Lla", ":Laravel artisan<cr>", desc = "List all artisan commands" },
-        { "<leader>Llr", ":Laravel routes<cr>",  desc = "List all routes" },
-        { "<leader>Llm", ":Laravel related<cr>", desc = "List all related files" },
-    },
-    cond = vim.fn.isdirectory("vendor") == 1,
-    opts = {},
-    config = true,
+	"adalessa/laravel.nvim",
+	dependencies = {
+		"tpope/vim-dotenv",
+		"MunifTanjim/nui.nvim",
+		"kevinhwang91/promise-async",
+		"nvim-telescope/telescope.nvim",
+	},
+	cmd = { "Laravel" },
+	opts = {
+		features = {
+			model_info = {
+				enable = false,
+			},
+			route_info = {
+				enable = false,
+			},
+		},
+	},
+	config = true,
 }
