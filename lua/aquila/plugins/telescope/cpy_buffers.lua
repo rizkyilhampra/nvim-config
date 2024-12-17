ad3959ee7965c392ed2979f4d7d47d532b7d1886
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-12-17 13:30:17 +0800

    style(cpy_buffers): reformat using stylua

--- lua/aquila/plugins/telescope/cpy_buffers.lua
+++ lua/aquila/plugins/telescope/cpy_buffers.lua
@@ -1,14 +1,14 @@
 return {
-    'adia-dev/cpy_buffers.nvim',
-    dependencies = {
-        'nvim-telescope/telescope.nvim',
-    },
-    keys = {
-        {
-            "<Leader>fc",
-            '<cmd>lua require("cpy_buffers.picker").open_file_picker<CR>',
-            desc = 'Copy from buffer to file'
-        },
-    },
-    opts = {},
+	"adia-dev/cpy_buffers.nvim",
+	dependencies = {
+		"nvim-telescope/telescope.nvim",
+	},
+	keys = {
+		{
+			"<Leader>fc",
+			'<cmd>lua require("cpy_buffers.picker").open_file_picker()<CR>',
+			desc = "Copy from buffer to file",
+		},
+	},
+	opts = {},
 }
