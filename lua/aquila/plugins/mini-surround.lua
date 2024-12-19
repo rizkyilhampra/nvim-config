d844dc87113ec97d609e0a56e1825055bbe1a444
rizkyilhampra <rizkyilhamp16@gmail.com>
2024-12-19 14:34:32 +0800

    chore(mini-surround): added usage note and silent to true

--- lua/aquila/plugins/mini-surround.lua
+++ lua/aquila/plugins/mini-surround.lua
@@ -1,6 +1,14 @@
+--- NOTE: Usage example
+--- Add surrounding quoetes:  sa + motion + char => saiw'
+--- Replace surrounding quoetes: sr + char => sr"'
+--- Delete surrounding quoetes: sd + char => sd'
+
 return {
-    'echasnovski/mini.surround',
-    event = "User BaseFile",
-    version = false,
-    opts = {}
+	"echasnovski/mini.surround",
+	event = "User BaseFile",
+	version = false,
+	opts = {
+		silent = true,
+	},
 }
+
