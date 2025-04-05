f5e814df0947b4e49574e01070dabcbb0d20bbf9
rizkyilhampra <rizkyilhamp16@gmail.com>
2025-04-05 17:11:06 +0800

    chore(dropbar): tagged to v12.0.2

--- lua/aquila/plugins/dropbar.lua
+++ lua/aquila/plugins/dropbar.lua
@@ -1,8 +1,9 @@
 return {
-    'Bekaboo/dropbar.nvim',
-    dependencies = {
-        'nvim-telescope/telescope-fzf-native.nvim'
-    },
-    event = "User BaseFile",
-    opts = {}
+	"Bekaboo/dropbar.nvim",
+	tag = "v12.0.2",
+	dependencies = {
+		"nvim-telescope/telescope-fzf-native.nvim",
+	},
+	event = "User BaseFile",
+	opts = {},
 }
