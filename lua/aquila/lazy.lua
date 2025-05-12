5998a57132a63e780647fa9c4b1573da1d7f6957
rizkyilhampra <rizkyilhamp16@gmail.com>
2025-05-12 21:18:03 +0800

    style(lazy): updated formatting

--- lua/aquila/lazy.lua
+++ lua/aquila/lazy.lua
@@ -1,66 +1,62 @@
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-    vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable", -- latest stable release
-        lazypath,
-    })
+	vim.fn.system({
+		"git",
+		"clone",
+		"--filter=blob:none",
+		"https://github.com/folke/lazy.nvim.git",
+		"--branch=stable",
+		lazypath,
+	})
 end
 
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup(
-    {
-        { import = "aquila.plugins" },
-        { import = "aquila.plugins.colorscheme" },
-        { import = "aquila.plugins.lsp" },
-        { import = "aquila.plugins.telescope" },
-        { import = "aquila.plugins.markdown" },
-        { import = "aquila.plugins.php" }
-    },
-    {
-        install = {
-            colorscheme = { "tokyonight" }
-        },
-        change_detection = {
-            notify = true
-        },
-        defaults = { lazy = true },
-        performance = {
-            rtp = {
-                -- disable some rtp plugins
-                disabled_plugins = {
-                    "gzip",
-                    "matchit",
-                    "matchparen",
-                    "netrwPlugin",
-                    "tarPlugin",
-                    "tohtml",
-                    "tutor",
-                    "zipPlugin",
-                },
-            }
-        },
-        ui = {
-            border = "rounded",
-            icons = vim.g.have_nerd_font and {} or {
-                cmd = '⌘',
-                config = '🛠',
-                event = '📅',
-                ft = '📂',
-                init = '⚙',
-                keys = '🗝',
-                plugin = '🔌',
-                runtime = '💻',
-                require = '🌙',
-                source = '📄',
-                start = '🚀',
-                task = '📌',
-                lazy = '💤 ',
-            },
-        }
-    }
-)
+require("lazy").setup({
+	{ import = "aquila.plugins" },
+	{ import = "aquila.plugins.colorscheme" },
+	{ import = "aquila.plugins.lsp" },
+	{ import = "aquila.plugins.telescope" },
+	{ import = "aquila.plugins.markdown" },
+	{ import = "aquila.plugins.php" },
+}, {
+	install = {
+		colorscheme = { "tokyonight" },
+	},
+	change_detection = {
+		notify = true,
+	},
+	defaults = { lazy = true },
+	performance = {
+		rtp = {
+			disabled_plugins = {
+				"gzip",
+				"matchit",
+				"matchparen",
+				"netrwPlugin",
+				"tarPlugin",
+				"tohtml",
+				"tutor",
+				"zipPlugin",
+			},
+		},
+	},
+	ui = {
+		border = "rounded",
+		icons = vim.g.have_nerd_font and {} or {
+			cmd = "⌘",
+			config = "🛠",
+			event = "📅",
+			ft = "📂",
+			init = "⚙",
+			keys = "🗝",
+			plugin = "🔌",
+			runtime = "💻",
+			require = "🌙",
+			source = "📄",
+			start = "🚀",
+			task = "📌",
+			lazy = "💤 ",
+		},
+	},
+})
