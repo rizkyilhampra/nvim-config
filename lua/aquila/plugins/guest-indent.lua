674531b4ffbb75a171a858966d27cce076b94601
rizkyilhampra <rizkyilhamp16@gmail.com>
2025-06-01 04:05:40 +0800

    refactor(guest-indent): removed explicit setup call

--- lua/aquila/plugins/guest-indent.lua
+++ lua/aquila/plugins/guest-indent.lua
@@ -1,8 +1,5 @@
 return {
-    "NMAC427/guess-indent.nvim",
-    event = "User BaseFile",
-    opts = {},
-    config = function(_, opts)
-        require("guess-indent").setup(opts)
-    end,
+	"NMAC427/guess-indent.nvim",
+	event = "User BaseFile",
+	opts = {},
 }
